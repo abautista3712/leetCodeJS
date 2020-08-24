6b0541003456f958f7ce9b2436f25428faf6038a
Abelard Bautista <abelardbautista@gmail.com>
2020-08-23 23:10:31 -0700

    Duplicates are pushed to a separate array and then printed to console outside of for loop.

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,9 +1,13 @@
 const arr = [1, 1, 0, 1, 1, 1];
 let a = 0;
 let b = a + 1;
+let duplicates = [];
+// let arr2 = [];
 
 for (a; a < arr.length; a++) {
   if (arr[a] == arr[b]) {
-    console.log(a);
+    duplicates.push(a);
   }
 }
+
+console.log(duplicates);
