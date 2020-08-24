fc492f32b8bc1e7ee764e847189fd32af476a54a
Abelard Bautista <abelardbautista@gmail.com>
2020-08-23 22:17:32 -0700

    Prints arr position when var a equals var b.

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -2,12 +2,8 @@ const arr = [1, 1, 0, 1, 1, 1];
 let a = 0;
 let b = a + 1;
 
-console.log("---arr[a]---");
 for (a; a < arr.length; a++) {
-  console.log(arr[a]);
-}
-
-console.log("---arr[b]---");
-for (b; b < arr.length; b++) {
-  console.log(arr[b]);
+  if (arr[a] == arr[b]) {
+    console.log(a);
+  }
 }
