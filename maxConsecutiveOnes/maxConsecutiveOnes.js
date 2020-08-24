222b128def041d526b0919bbf35a29b2d44cca24
Abelard Bautista <abelardbautista@gmail.com>
2020-08-23 21:43:38 -0700

    Added example array. Variable 'a' prints all array values.

---
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -0,0 +1,11 @@
+const arr = [1, 1, 0, 1, 1, 1];
+let a;
+// let b = a + 1;
+
+for (a = 0; a < arr.length; a++) {
+  console.log(arr[a]);
+}
+
+// for (b = 1; b < arr.length; b++) {
+//   console.log(arr[b]);
+// }
