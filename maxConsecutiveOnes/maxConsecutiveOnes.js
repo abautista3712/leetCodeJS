7bdaf88b5ca33a771304004f70aed78eea7995a3
Abelard Bautista <abelardbautista@gmail.com>
2020-08-24 19:16:20 -0700

    Algorithm counts max consecutive ones when max set is rightmost.

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,10 +1,18 @@
 const arr = [1, 1, 0, 1, 1, 1];
-let count = 0;
+let count = 1;
+let max;
+
+console.log("---Initial value of 'count'---");
+console.log(count);
 
 for (let a = 0; a < arr.length; a++) {
-  if (arr[a] === arr[a + 1] && arr[a] === 1) {
+  if (arr[a] === 1 && arr[a] === arr[a + 1]) {
     count++;
+    console.log("---1 found. Increase 'count'---");
     console.log(count);
+  } else if (arr[a] === 0) {
+    console.log("---No 1's found. 'count' reset to 1---");
+    count = 1;
   }
 }
 
