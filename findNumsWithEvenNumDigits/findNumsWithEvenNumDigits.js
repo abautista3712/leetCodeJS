d44315d48ffd8f94d26197a637a43b58def8deba
Abelard Bautista <abelardbautista@gmail.com>
2020-08-25 00:42:51 -0700

    for loop 'a' loops through example 1 arr and counts numbers with an even number of digits.

--- findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
+++ findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
@@ -4,18 +4,24 @@ let count = 0;
 let even = 0;
 
 // Counting digits from nums[0]
-// for (let a = 0; a < nums.length; a++) {
-for (let b = 0; b < 6; b++) {
-  if (Math.floor(nums[1] / Math.pow(10, b)) !== 0) {
-    console.log(`---Math.floor(nums[1] / Math.pow(10, ${b}))---`);
-    console.log(Math.floor(nums[1] / Math.pow(10, b)));
-    count++;
+for (let a = 0; a < nums.length; a++) {
+  console.log("Checking if number of digits is even");
+  if (count % 2 === 0) {
+    even++;
   }
-}
-// }
-
-if (count % 2 === 0) {
-  even++;
+  console.log(even);
+  console.log("Resetting count to 0:");
+  count = 0;
+  console.log(count);
+  for (let b = 0; b < 6; b++) {
+    if (Math.floor(nums[a] / Math.pow(10, b)) !== 0) {
+      console.log(`---Math.floor(nums[${a}] / Math.pow(10, ${b}))---`);
+      console.log(Math.floor(nums[a] / Math.pow(10, b)));
+      console.log("Adding 1 to count:");
+      count++;
+      console.log(count);
+    }
+  }
 }
 
 console.log("---count---");
