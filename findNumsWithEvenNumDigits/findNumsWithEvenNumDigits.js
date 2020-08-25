0f5ce168f7a911d9cd4c90b2cac11331500f1451
Abelard Bautista <abelardbautista@gmail.com>
2020-08-25 00:18:03 -0700

    Adds 1 to 'even' variable if number digits are divisible by 2 without remainder.

--- findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
+++ findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
@@ -1,15 +1,24 @@
 // Testing Example 1
 const nums = [12, 345, 2, 6, 7896];
 let count = 0;
+let even = 0;
 
 // Counting digits from nums[0]
-for (let a = 0; a < 6; a++) {
-  if (Math.floor(nums[0] / Math.pow(10, a)) !== 0) {
-    console.log(`---Math.floor(nums[0] / Math.pow(10, ${a}))---`);
-    console.log(Math.floor(nums[0] / Math.pow(10, a)));
+// for (let a = 0; a < nums.length; a++) {
+for (let b = 0; b < 6; b++) {
+  if (Math.floor(nums[1] / Math.pow(10, b)) !== 0) {
+    console.log(`---Math.floor(nums[1] / Math.pow(10, ${b}))---`);
+    console.log(Math.floor(nums[1] / Math.pow(10, b)));
     count++;
   }
+}
+// }
+
+if (count % 2 === 0) {
+  even++;
 }
 
 console.log("---count---");
 console.log(count);
+console.log("---even---");
+console.log(even);
