a67b6611139a754f6ee25153e0f50ca55521fd07
Abelard Bautista <abelardbautista@gmail.com>
2020-08-24 18:56:54 -0700

    Changed if statement to look at adjacent elements. Added count.

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,13 +1,11 @@
 const arr = [1, 1, 0, 1, 1, 1];
-let a = 0;
-let b = a + 1;
-let duplicates = [];
-// let arr2 = [];
+let count = 0;
 
-for (a; a < arr.length; a++) {
-  if (arr[a] == arr[b]) {
-    duplicates.push(a);
+for (let a = 0; a < arr.length; a++) {
+  if (arr[a] === arr[a + 1] && arr[a] === 1) {
+    count++;
+    console.log(count);
   }
 }
 
-console.log(duplicates);
+console.log(count);
