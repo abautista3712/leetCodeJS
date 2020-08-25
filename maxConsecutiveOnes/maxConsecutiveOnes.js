80b07fb6c2283182366b914ef119d7962f605922
Abelard Bautista <abelardbautista@gmail.com>
2020-08-24 22:51:45 -0700

    Solution accepted by LC.

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,20 +1,20 @@
-// SOLVED
 const nums = Array.from({ length: Math.floor(Math.random() * 10001) }, () =>
   Math.floor(Math.random() * 2)
 );
+
 console.log(`Array of ${nums.length} items:`);
 console.log(nums);
 
 const findMaxConsecutiveOnes = (nums) => {
   let count = 1;
-  let max = 0;
+  let max = 1;
 
   for (let a = 0; a < nums.length; a++) {
     if (!nums.includes(1)) {
-      count = 0;
+      max = 0;
     } else if (nums[a] === 1 && nums[a] === nums[a + 1]) {
       count++;
-      if (count > max) {
+      if (count >= max) {
         max = count;
       }
     } else if (nums[a] === 0) {
