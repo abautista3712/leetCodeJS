ba2e14385b4a6fd1cedb259a745d073f6f1285eb
Abelard Bautista <abelardbautista@gmail.com>
2020-08-25 00:05:35 -0700

    Consolidated code using exponents (Math.pow(x,y)).

--- findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
+++ findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
@@ -3,35 +3,13 @@ const nums = [12, 345, 2, 6, 7896];
 let count = 0;
 
 // Counting digits from nums[0]
-if (Math.floor(nums[0] / 1) !== 0) {
-  console.log("---Math.floor(nums[0] / 1)---");
-  console.log(Math.floor(nums[0] / 1));
-  count++;
+for (let a = 0; a < 6; a++) {
+  if (Math.floor(nums[0] / Math.pow(10, a)) !== 0) {
+    console.log(`---Math.floor(nums[0] / Math.pow(10, ${a}))---`);
+    console.log(Math.floor(nums[0] / Math.pow(10, a)));
+    count++;
+  }
 }
-if (Math.floor(nums[0] / 10) !== 0) {
-  console.log("---Math.floor(nums[0] / 10)---");
-  console.log(Math.floor(nums[0] / 10));
-  count++;
-}
-if (Math.floor(nums[0] / 100) !== 0) {
-  console.log("---Math.floor(nums[0] / 100)---");
-  console.log(Math.floor(nums[0] / 100));
-  count++;
-}
-if (Math.floor(nums[0] / 1000) !== 0) {
-  console.log("---Math.floor(nums[0] / 1000)---");
-  console.log(Math.floor(nums[0] / 1000));
-  count++;
-}
-if (Math.floor(nums[0] / 10000) !== 0) {
-  console.log("---Math.floor(nums[0] / 10000)---");
-  console.log(Math.floor(nums[0] / 10000));
-  count++;
-}
-if (Math.floor(nums[0] / 100000) !== 0) {
-  console.log("---Math.floor(nums[0] / 100000)---");
-  console.log(Math.floor(nums[0] / 100000));
-  count++;
-}
+
 console.log("---count---");
 console.log(count);
