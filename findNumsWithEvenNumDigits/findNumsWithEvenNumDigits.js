e66e7020202d8fa3e8f73d4fd072bea730f06853
Abelard Bautista <abelardbautista@gmail.com>
2020-08-25 01:23:47 -0700

    Console.logs commented out (deleted on LC) for optimization.

--- findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
+++ findNumsWithEvenNumDigits/findNumsWithEvenNumDigits.js
@@ -16,41 +16,41 @@ const findNumbers = (nums) => {
   let even = 0;
 
   for (let a = 0; a < nums.length; a++) {
-    console.log("Checking if number of digits is even...");
+    // console.log("Checking if number of digits is even...");
     if (count > 0 && count % 2 === 0) {
-      console.log("Number has even digits. Adding 1 to 'even'...");
+      //   console.log("Number has even digits. Adding 1 to 'even'...");
       even++;
     }
-    console.log("Check complete...");
-    console.log("---even---");
-    console.log(even);
-    console.log("Resetting 'count' to 0...");
-    console.log("---count---");
+    // console.log("Check complete...");
+    // console.log("---even---");
+    // console.log(even);
+    // console.log("Resetting 'count' to 0...");
+    // console.log("---count---");
     count = 0;
-    console.log(count);
+    // console.log(count);
     for (let b = 0; b < 6; b++) {
       if (Math.floor(nums[a] / Math.pow(10, b)) !== 0) {
-        console.log(`---Math.floor(nums[${a}] / Math.pow(10, ${b}))---`);
-        console.log(Math.floor(nums[a] / Math.pow(10, b)));
-        console.log("Adding 1 to 'count'...");
+        // console.log(`---Math.floor(nums[${a}] / Math.pow(10, ${b}))---`);
+        // console.log(Math.floor(nums[a] / Math.pow(10, b)));
+        // console.log("Adding 1 to 'count'...");
         count++;
-        console.log("---count---");
-        console.log(count);
+        // console.log("---count---");
+        // console.log(count);
       }
     }
   }
 
-  console.log("Final check if number of digits is even...");
+  //   console.log("Final check if number of digits is even...");
   if (count > 0 && count % 2 === 0) {
-    console.log("Number has even digits. Adding 1 to 'even'...");
+    // console.log("Number has even digits. Adding 1 to 'even'...");
     even++;
   }
-  console.log("Check complete...");
+  //   console.log("Check complete...");
 
-  console.log("---final count---");
-  console.log(count);
-  console.log("---final even---");
-  console.log(even);
+  //   console.log("---final count---");
+  //   console.log(count);
+  //   console.log("---final even---");
+  //   console.log(even);
   return even;
 };
 findNumbers(nums);
