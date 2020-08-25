5910f7c0ac9b197ccfb850d477ef19ea13fb3726
Abelard Bautista <abelardbautista@gmail.com>
2020-08-24 19:52:04 -0700

    Added condition when no 1's are present in array.

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,4 +1,4 @@
-const arr = [1, 1, 0, 1, 1, 1];
+const arr = [0, 0, 0, 0, 0, 1, 1, 1];
 let count = 1;
 let max;
 
@@ -6,12 +6,14 @@ console.log("---Initial value of 'count'---");
 console.log(count);
 
 for (let a = 0; a < arr.length; a++) {
-  if (arr[a] === 1 && arr[a] === arr[a + 1]) {
+  if (!arr.includes(1)) {
+    console.log("---No 1's present in array. Count is 0---");
+    count = 0;
+  } else if (arr[a] === 1 && arr[a] === arr[a + 1]) {
+    console.log("---1's present in array: 1 found. Increase 'count' by 1---");
     count++;
-    console.log("---1 found. Increase 'count'---");
-    console.log(count);
   } else if (arr[a] === 0) {
-    console.log("---No 1's found. 'count' reset to 1---");
+    console.log("---1's present in array: 0 found. 'count' reset to 1---");
     count = 1;
   }
 }
