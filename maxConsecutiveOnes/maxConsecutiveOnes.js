37083f12ea2a57582473d67d1b34bb38d129254f
Abelard Bautista <abelardbautista@gmail.com>
2020-08-24 22:13:39 -0700

    Max consecutives ones are printed in let variable 'max'

--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,6 +1,6 @@
-const arr = [0, 0, 0, 0, 0, 1, 1, 1];
+const arr = [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1];
 let count = 1;
-let max;
+let max = 0;
 
 console.log("---Initial value of 'count'---");
 console.log(count);
@@ -12,10 +12,21 @@ for (let a = 0; a < arr.length; a++) {
   } else if (arr[a] === 1 && arr[a] === arr[a + 1]) {
     console.log("---1's present in array: 1 found. Increase 'count' by 1---");
     count++;
+    console.log("---'count' inside for loop---");
+    console.log(count);
+    console.log("---'max' inside for loop---");
+    console.log(max);
+    if (count > max) {
+      max = count;
+    }
   } else if (arr[a] === 0) {
     console.log("---1's present in array: 0 found. 'count' reset to 1---");
     count = 1;
   }
 }
 
+console.log("---count---");
 console.log(count);
+
+console.log("---max---");
+console.log(max);
