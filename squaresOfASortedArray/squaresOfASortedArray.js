0c9650cd3917253cf3ddd02bc488c5a73eb1da70
Abelard Bautista <abelardbautista@gmail.com>
2020-08-26 22:29:23 -0700

    Added empty arr to hold squares.

--- squaresOfASortedArray/squaresOfASortedArray.js
+++ squaresOfASortedArray/squaresOfASortedArray.js
@@ -1,13 +1,15 @@
 // Testing Example 1
 // Expected Output: [0,1,9,16,100]
 const A = [-4, -1, 0, 3, 10];
+const Squares = [];
 
 const sortedSquares = (A) => {
-  //   for (let i = 0; i < A.length; i++) {
-  //     console.log(A[i]);
-  //   }
-  A.sort((a, b) => a - b);
-  console.log(A);
+  for (let i = 0; i < A.length; i++) {
+    Squares.push(Math.pow(A[i], 2));
+  }
+  //   A.sort((a, b) => a - b);
+  //   console.log(A);
 };
 
 sortedSquares(A);
+console.log(Squares);
