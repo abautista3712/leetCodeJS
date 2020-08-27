b2ffba8c465c0a8aeb875147bf23134a0906b2a1
Abelard Bautista <abelardbautista@gmail.com>
2020-08-26 22:18:32 -0700

    Added sort function that sorts by increasing num.

--- squaresOfASortedArray/squaresOfASortedArray.js
+++ squaresOfASortedArray/squaresOfASortedArray.js
@@ -3,9 +3,11 @@
 const A = [-4, -1, 0, 3, 10];
 
 const sortedSquares = (A) => {
-  for (let i = 0; i < A.length; i++) {
-    console.log(A[i]);
-  }
+  //   for (let i = 0; i < A.length; i++) {
+  //     console.log(A[i]);
+  //   }
+  A.sort((a, b) => a - b);
+  console.log(A);
 };
 
 sortedSquares(A);
