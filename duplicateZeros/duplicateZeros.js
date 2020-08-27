4e07fd39fc1d0426bc006f77c2f7d00290a37ed5
Abelard Bautista <abelardbautista@gmail.com>
2020-08-26 23:30:31 -0700

    Added while loop.

--- duplicateZeros/duplicateZeros.js
+++ duplicateZeros/duplicateZeros.js
@@ -1,3 +1,15 @@
-const arr = [];
+// Testing Example 1
+// Input: [1,0,2,3,0,4,5,0]
+// Output: null
+// Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+const arr = [1, 0, 2, 3, 0, 4, 5, 0];
 
-const duplicateZeros = (arr) => {};
+const duplicateZeros = (arr) => {
+  let i = 0;
+  while (i < arr.length) {
+    console.log(i);
+    i++;
+  }
+};
+
+duplicateZeros(arr);
