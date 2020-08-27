0557746909017d902ede9e4022d5029f9731c06a
Abelard Bautista <abelardbautista@gmail.com>
2020-08-26 23:12:24 -0700

    Solution accepted by LC.

--- squaresOfASortedArray/squaresOfASortedArray.js
+++ squaresOfASortedArray/squaresOfASortedArray.js
@@ -1,14 +1,24 @@
 // Testing Example 1
-// Expected Output: [0,1,9,16,100]
+// Input: [-4, -1, 0, 3, 10]
+// Output Success: [0,1,9,16,100]
+
+// Testing Example 2
+// Input: [-7,-3,2,3,11]
+// Output Success: [4,9,9,49,121]
+
+// Test Case
+// Input: [367, 4, 29, 72, 5]
+
 const A = [-4, -1, 0, 3, 10];
-const Squares = [];
 
 const sortedSquares = (A) => {
+  const Squares = [];
   for (let i = 0; i < A.length; i++) {
     Squares.push(Math.pow(A[i], 2));
   }
   Squares.sort((a, b) => a - b);
-  console.log(Squares);
+  //   console.log(Squares);
+  return Squares;
 };
 
 sortedSquares(A);
