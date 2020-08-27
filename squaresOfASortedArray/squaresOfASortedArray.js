6a49623ac4c84ed9a4176e372735be21ee145b0a
Abelard Bautista <abelardbautista@gmail.com>
2020-08-26 22:07:48 -0700

    Added test input from example 1. Function reads array and prints each num to console.

--- squaresOfASortedArray/squaresOfASortedArray.js
+++ squaresOfASortedArray/squaresOfASortedArray.js
@@ -1,3 +1,11 @@
-const A = [];
+// Testing Example 1
+// Expected Output: [0,1,9,16,100]
+const A = [-4, -1, 0, 3, 10];
 
-const sortedSquares = (A) => {};
+const sortedSquares = (A) => {
+  for (let i = 0; i < A.length; i++) {
+    console.log(A[i]);
+  }
+};
+
+sortedSquares(A);
