15b3739f48dffa5ec00b19c7592966e323c288f0
Abelard Bautista <abelardbautista@gmail.com>
2020-08-29 21:00:03 -0700

    Loop through arr and push index of zeroes to separate arr.

--- duplicateZeros/duplicateZeros.js
+++ duplicateZeros/duplicateZeros.js
@@ -3,19 +3,27 @@
 // Output: null
 // Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
 const arr = [1, 0, 2, 3, 0, 4, 5, 0];
-const zeroesIndexes = [];
+const zeroes = [];
 
 const duplicateZeros = (arr) => {
   let i = 0;
   while (i < arr.length) {
     if (arr[i] === 0) {
-      zeroesIndexes.push(i);
+      zeroes.push(i);
     }
+    // zeroes.push("test");
+    // arr.splice(testArr[i], 0, "test");
+    // console.log(arr);
     i++;
   }
 };
 
 duplicateZeros(arr);
+console.log(zeroes);
 
-console.log(`arr ${arr}`);
-console.log(`zeroesIndex ${zeroesIndexes}`);
+// for (let j = 0; j < zeroesIndexes.length; j++) {
+//   arr.splice(1, 0, `Inserted at j=${j}`);
+// }
+
+// console.log(arr);
+// console.log(`arr.reverse() ${arr.reverse()}`);
