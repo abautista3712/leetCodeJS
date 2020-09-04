a03f0203c02628dde9288952baa75ac832abce20
Abelard Bautista <abelardbautista@gmail.com>
2020-09-03 22:22:02 -0700

    Added empty variables for nums1, m, nums2, and n.

--- mergeSortedArray/mergeSortedArray.js
+++ mergeSortedArray/mergeSortedArray.js
@@ -6,6 +6,11 @@
 //  * @return {void} Do not return anything, modify nums1 in-place instead.
 //  */
 
+const nums1 = [];
+const m = nums1.length;
+const nums2 = [];
+const n = nums2.length;
+
 const merge = (nums1, m, nums2, n) => {
   console.log("Test");
 };
