13b77176a6ba30425b17fdbcd4e44032902661b8
Abelard Bautista <abelardbautista@gmail.com>
2020-09-04 18:20:48 -0700

    Function loops through and removes all 0 value integers.

--- mergeSortedArray/mergeSortedArray.js
+++ mergeSortedArray/mergeSortedArray.js
@@ -6,13 +6,21 @@
 //  * @return {void} Do not return anything, modify nums1 in-place instead.
 //  */
 
-const nums1 = [];
+const nums1 = [1, 2, 3, 0, 0, 0];
 const m = nums1.length;
-const nums2 = [];
+const nums2 = [2, 5, 6];
 const n = nums2.length;
 
 const merge = (nums1, m, nums2, n) => {
-  console.log("Test");
+  let i = m;
+  while (i >= 0) {
+    if (nums1[i] === 0) {
+      nums1.splice(i, 1);
+    }
+    i--;
+  }
 };
 
-merge();
+merge(nums1, m, nums2, n);
+console.log("---nums1 after function call---");
+console.log(nums1);
