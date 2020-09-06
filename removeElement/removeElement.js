8d4d97878c37689121e49843520e25687cf269ef
Abelard Bautista <abelardbautista@gmail.com>
2020-09-06 00:11:57 -0700

    Added array sort to reduce memory usage. Solution accepted by LC.

--- removeElement/removeElement.js
+++ removeElement/removeElement.js
@@ -8,12 +8,15 @@ const nums = [0, 1, 2, 2, 3, 0, 4, 2];
 const val = 2;
 
 const removeElement = (nums, val) => {
+  nums.sort((a, b) => a - b);
   let i = nums.length;
   while (i >= 0) {
-    if (nums[i] === val) {
+    if (nums[i] != val) {
+      i--;
+    } else if (nums[i] === val) {
       nums.splice(i, 1);
+      i--;
     }
-    i--;
   }
 };
 
