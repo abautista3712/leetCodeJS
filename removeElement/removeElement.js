e615328db81792064b56c1a2e3b5cb431e3b7928
Abelard Bautista <abelardbautista@gmail.com>
2020-09-05 23:48:19 -0700

    Test example one success.

--- removeElement/removeElement.js
+++ removeElement/removeElement.js
@@ -4,4 +4,18 @@
 //  * @return {number}
 //  */
 
-const removeElement = (nums, val) => {};
+const nums = [3, 2, 2, 3];
+const val = 3;
+
+const removeElement = (nums, val) => {
+  let i = nums.length;
+  while (i >= 0) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+    i--;
+  }
+};
+
+removeElement(nums, val);
+console.log(nums);
