ec8ae70614e8a7861ba431c3e4973adb0990bff2
Abelard Bautista <abelardbautista@gmail.com>
2020-09-07 02:44:58 -0700

    Function removes any values from nums arr that appear in uniqueVals arr.

--- removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
+++ removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
@@ -3,17 +3,20 @@
 //  * @return {number}
 //  */
 
-const nums = [1, 1, 2];
-const uniqueVals = [1];
+const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+const uniqueVals = [];
 
 const removeDuplicates = (nums) => {
   let i = nums.length - 1;
   while (i >= 0) {
     if (uniqueVals.length === 0) {
+      uniqueVals.push(nums[i]);
       i--;
     } else if (uniqueVals.includes(nums[i])) {
+      nums.splice(i, 1);
       i--;
     } else {
+      console.log("push");
       uniqueVals.push(nums[i]);
       i--;
     }
