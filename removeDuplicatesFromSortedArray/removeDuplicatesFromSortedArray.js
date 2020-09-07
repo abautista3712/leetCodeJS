4977c0c7cf8025dd8b3b72efcbcc3fff3536b4c6
Abelard Bautista <abelardbautista@gmail.com>
2020-09-07 02:18:59 -0700

    Function checks for unique values and pushes them to uniqueVals arr.

--- removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
+++ removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
@@ -3,4 +3,25 @@
 //  * @return {number}
 //  */
 
-const removeDuplicates = (nums) => {};
+const nums = [1, 1, 2];
+const uniqueVals = [1];
+
+const removeDuplicates = (nums) => {
+  let i = nums.length - 1;
+  while (i >= 0) {
+    if (uniqueVals.length === 0) {
+      i--;
+    } else if (uniqueVals.includes(nums[i])) {
+      i--;
+    } else {
+      uniqueVals.push(nums[i]);
+      i--;
+    }
+  }
+};
+
+removeDuplicates(nums);
+console.log("---nums---");
+console.log(nums);
+console.log("---uniqueVals---");
+console.log(uniqueVals);
