ee647e6330f6b967ce91e0444c64e0ab06c5ee3f
Abelard Bautista <abelardbautista@gmail.com>
2020-09-11 02:04:27 -0700

    Rewrote binarySearch for learning purposes. Added console logs for clarity.

--- checkIfNAndItsDoubleExist/checkIfNAndItsDoubleExist.js
+++ checkIfNAndItsDoubleExist/checkIfNAndItsDoubleExist.js
@@ -3,23 +3,54 @@
 //  * @return {boolean}
 //  */
 
-// const arr = [10, 2, 5, 3];
+const arr = [10, 2, 5, 3];
+const N = 5;
+
+let binarySearch = (arr, N) => {
+  let startIndex = 0;
+  let endIndex = arr.length - 1;
+  while (startIndex <= endIndex) {
+    let midIndex = Math.floor(startIndex + endIndex / 2);
+    console.log("---startIndex---");
+    console.log(startIndex);
+    console.log("---endIndex---");
+    console.log(endIndex);
+    console.log("---midIndex---");
+    console.log(midIndex);
+    console.log("---Does arr[midIndex] === N?---");
+    console.log(`arr[midIndex]: ${arr[midIndex]}`);
+    console.log(`N: ${N}`);
+    if (arr[midIndex] === N) return true;
+    else if (arr[midIndex] < N) {
+      startIndex = midIndex + 1;
+    } else endIndex = midIndex - 1;
+  }
+};
 
 // const checkIfExist = (arr) => {
-let iterativeFunction = (arr, x) => {
-  let start = 0,
-    end = arr.length - 1;
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
-    if (arr[mid] === x) return true;
-    else if (arr[mid] < x) start = mid + 1;
-    else end = mid - 1;
-  }
-  return false;
-};
+// let iterativeFunction = (arr, N) => {
+//   let startIndex = 0;
+//   let endIndex = arr.length - 1;
+//   while (startIndex <= endIndex) {
+//     let midIndex = Math.floor((startIndex + endIndex) / 2);
+//     console.log("---startIndex---");
+//     console.log(startIndex);
+//     console.log("---endIndex---");
+//     console.log(endIndex);
+//     console.log("---midIndex---");
+//     console.log(midIndex);
+//     console.log(arr[midIndex]);
+//     if (arr[midIndex] === N) return true;
+//     else if (arr[midIndex] < N) startIndex = midIndex + 1;
+//     else endIndex = midIndex - 1;
+//   }
+//   return false;
+// };
 // };
 
-let arr = [1, 3, 5, 7, 8, 9];
-let x = 5;
+// let arr = [1, 3, 5, 7, 8, 9];
+// let N = 3;
 
-if (iterativeFunction(arr, x, 0, arr.length - 1)) console.log("Element found!");
+binarySearch(arr, N) ? console.log("true") : console.log("false");
+
+// if (iterativeFunction(arr, x, 0, arr.length - 1)) console.log("Element found!");
