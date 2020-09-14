78d32d6d87c5fc406814c1baba52677b6d027e00
Abelard Bautista <abelardbautista@gmail.com>
2020-09-13 23:06:09 -0700

    Debugging for LC.

--- checkIfNAndItsDoubleExist/checkIfNAndItsDoubleExist.js
+++ checkIfNAndItsDoubleExist/checkIfNAndItsDoubleExist.js
@@ -3,17 +3,15 @@
 //  * @return {boolean}
 //  */
 
-const arr = [7, 1, 14, 11];
-// let M = 0;
-// let N = 2 * M;
+const arr = [10, 2, 5, 3];
 
-let binarySearch = (arr, M) => {
-  console.log("---arr BEFORE sort()---");
-  console.log(arr);
+let checkIfExist = (arr, M) => {
+  // console.log("---arr BEFORE sort()---");
+  // console.log(arr);
 
   arr.sort((a, b) => a - b);
-  console.log("---arr AFTER sort()---");
-  console.log(arr);
+  // console.log("---arr AFTER sort()---");
+  // console.log(arr);
 
   let startIndex = 0;
   let endIndex = arr.length - 1;
@@ -31,13 +29,18 @@ let binarySearch = (arr, M) => {
 };
 
 for (let i = 0; i < arr.length; i++) {
-  if (binarySearch(arr, arr[i])) {
+  if (checkIfExist(arr, arr[i])) {
+    console.log(`Checking for value M = ${arr[i]}...`);
     console.log(`M = ${arr[i]} exists in arr`);
-    if (binarySearch(arr, arr[i] * 2)) {
-      console.log(`N = ${arr[i] * 2} exists in arr`);
+    if (checkIfExist(arr, arr[i] * 2)) {
+      console.log(`N = ${arr[i] * 2} exists in arr. Ending search.`);
+      return true;
     } else {
       console.log(`N = ${arr[i] * 2} does NOT exist in arr`);
+      console.log("Checking next value of M...");
     }
   } else {
     console.log(`M = ${arr[i]} does NOT exist in arr`);
+    return false;
   }
+}
