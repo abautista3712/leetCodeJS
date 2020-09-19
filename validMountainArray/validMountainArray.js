50dfc84e4069a039ac57ca5c98445393705898cb
Abelard Bautista <abelardbautista@gmail.com>
2020-09-18 21:42:03 -0700

    Added array A and for loop.

---
+++ validMountainArray/validMountainArray.js
@@ -0,0 +1,14 @@
+// /**
+//  * @param {number[]} A
+//  * @return {boolean}
+//  */
+
+const A = [2, 1];
+
+const validMountainArray = (A) => {
+  for (let i = 0; i < A.length; i++) {
+    console.log("Test");
+  }
+};
+
+validMountainArray(A);
