46a436065baa34bc50a5a994991d7adbcf30ce83
Abelard Bautista <abelardbautista@gmail.com>
2020-09-18 22:15:56 -0700

    Function looks for increasing array and identifies max.

--- validMountainArray/validMountainArray.js
+++ validMountainArray/validMountainArray.js
@@ -3,13 +3,22 @@
 //  * @return {boolean}
 //  */
 
-const A = [2, 1];
+const A = [1, 3, 1];
 
 const validMountainArray = (A) => {
   for (let i = 0; i < A.length; i++) {
-    if (A[i + 1] < A[i]) {
-      console.log("---MAX Found---");
+    if (A[i + 1] === A[i]) {
+      console.log("---Idential values found. NOT a valid Mountain Array---");
+      return;
+    } else if (A[i + 1] > A[i]) {
+      console.log("Increasing...");
       console.log(A[i]);
+    } else if (A[i + 1] < A[i]) {
+      console.log("MAX value identified");
+      console.log(A[i]);
+    } else {
+      console.log("---No increase. NOT a valid Mountain Array---");
+      return;
     }
   }
 };
