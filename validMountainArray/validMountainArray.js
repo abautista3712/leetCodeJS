41b44da9ca57d5218aa883f3cd3976582c3aa19d
Abelard Bautista <abelardbautista@gmail.com>
2020-09-18 22:32:49 -0700

    Fx identifies decrease and stops at end of array.

--- validMountainArray/validMountainArray.js
+++ validMountainArray/validMountainArray.js
@@ -3,22 +3,25 @@
 //  * @return {boolean}
 //  */
 
-const A = [1, 3, 1];
+const A = [0, 3, 2, 1];
 
 const validMountainArray = (A) => {
   for (let i = 0; i < A.length; i++) {
     if (A[i + 1] === A[i]) {
       console.log("---Idential values found. NOT a valid Mountain Array---");
-      return;
+      return false;
     } else if (A[i + 1] > A[i]) {
       console.log("Increasing...");
       console.log(A[i]);
     } else if (A[i + 1] < A[i]) {
-      console.log("MAX value identified");
-      console.log(A[i]);
+      console.log("Decreasing...");
+      if ((i = A.length - 1)) {
+        console.log("---Valid Mountain Array---");
+        return true;
+      }
     } else {
       console.log("---No increase. NOT a valid Mountain Array---");
-      return;
+      return false;
     }
   }
 };
