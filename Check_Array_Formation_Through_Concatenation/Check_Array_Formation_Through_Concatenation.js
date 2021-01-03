f65c701136613d472e63f104c364d46df8095f65
Abelard Bautista <abelardbautista@gmail.com>
2021-01-03 03:58:39 -0800

    Create obj to preserve order of arr.

--- Check_Array_Formation_Through_Concatenation/Check_Array_Formation_Through_Concatenation.js
+++ Check_Array_Formation_Through_Concatenation/Check_Array_Formation_Through_Concatenation.js
@@ -3,10 +3,13 @@
  * @param {number[][]} pieces
  * @return {boolean}
  */
-const arr = [91,4,64,78]
-const pieces = [[78],[4,64],[91]]  
+const arr = [15,88]
+const pieces = [[88],[15]]  
+
+let orderedArrObj = {}
 
 var canFormArray = function(arr, pieces) {
+  // Return false if pieces[i][j] does not appear in 'arr'
   for (let i = 0; i < pieces.length; i++)
   {
     if (pieces[i].length == 1)
@@ -21,7 +24,6 @@ var canFormArray = function(arr, pieces) {
     {
       for (let j = 0; j < pieces[i].length; j++)
       {
-        // console.log(pieces[i][j])
         if (arr.includes(pieces[i][j]) == false)
         {
           console.log(arr.includes(pieces[i][j]))
@@ -30,6 +32,14 @@ var canFormArray = function(arr, pieces) {
       }
     }
   }
+
+  // Create object of 'arr' array with 'arr' array value as key and 'arr' array order as value.
+  for (let i = 0; i < arr.length; i++)
+  {
+    orderedArrObj[i] = arr[i]
+  } 
 };
-    
+
 canFormArray(arr,pieces)
+
+console.log(orderedArrObj)
