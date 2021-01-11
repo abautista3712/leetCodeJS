92912dda52304012987a4c4d2e68701c7d4f0353
Abelard Bautista <abelardbautista@gmail.com>
2021-01-11 02:41:06 -0800

    Added mergeSort.

--- Arrays 101/squaresOfASortedArray/2.js
+++ Arrays 101/squaresOfASortedArray/2.js
@@ -15,8 +15,33 @@ var sortedSquares = function (nums) {
     nums[i] = squareNums(nums[i]);
   }
 
-  console.log(nums);
   return nums;
 };
 
+const merge = (left, right) => {
+  const arr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+  return [...arr, ...left, ...right];
+};
+
+const mergeSort = (nums) => {
+  const half = nums.length / 2;
+
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  const left = nums.splice(0, half);
+
+  return merge(mergeSort(left), mergeSort(nums));
+};
+
 sortedSquares(nums);
+mergeSort(nums);
