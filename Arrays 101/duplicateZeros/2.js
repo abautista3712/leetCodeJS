6a43110fb84c18c53482b57b50e405ddf59f8942
Abelard Bautista <abelardbautista@gmail.com>
2021-01-19 17:05:04 -0800

    Insertion via decrementing while-loop with JS splice.

--- Arrays 101/duplicateZeros/2.js
+++ Arrays 101/duplicateZeros/2.js
@@ -2,4 +2,24 @@
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
  */
-var duplicateZeros = function (arr) {};
+const arr = [1, 0, 2, 3, 0, 4, 5, 0];
+
+var duplicateZeros = function (arr) {
+  let n = arr.length;
+
+  let i = arr.length - 1;
+  let insertionCount = 0;
+  while (i >= 0) {
+    if (arr[i] == 0) {
+      arr.splice(i, 0, 0);
+    }
+    insertionCount++;
+    i--;
+  }
+
+  arr.splice(n, insertionCount);
+
+  return arr;
+};
+
+console.log(duplicateZeros(arr));
