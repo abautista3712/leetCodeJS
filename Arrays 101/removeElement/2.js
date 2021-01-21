4092f5f5d9b4e5e0de8cb742af65b3165d256a0c
Abelard Bautista <abelardbautista@gmail.com>
2021-01-20 19:13:18 -0800

    Simplified code: find value and swap to last position then pop.

--- Arrays 101/removeElement/2.js
+++ Arrays 101/removeElement/2.js
@@ -7,72 +7,7 @@ const nums = [0, 1, 2, 2, 3, 0, 4, 2];
 const val = 2;
 
 var removeElement = function (nums, val) {
-  const swap = (arr, i, j) => {
-    const tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-  };
-
-  const quickSort = (arr, left, right) => {
-    const n = arr.length;
-
-    if (left >= right) {
-      return;
-    }
-
-    left = left || 0;
-    right = right || n - 1;
-
-    const index = partition(arr, left, right);
-
-    quickSort(arr, left, index - 1);
-    quickSort(arr, index, right);
-
-    return arr;
-  };
-
-  const partition = (arr, left, right) => {
-    const pivot = Math.floor((left + right) / 2);
-
-    while (left <= right) {
-      while (arr[left] < arr[pivot]) {
-        left++;
-      }
-      while (arr[right] > arr[pivot]) {
-        right--;
-      }
-
-      if (left <= right) {
-        swap(arr, left, right);
-        left++;
-        right--;
-      }
-    }
-    return left;
-  };
-  quickSort(nums);
-  console.log(nums);
-
-  const binarySearch = (arr, searchVal) => {
-    let startIndex = 0;
-    let endIndex = arr.length - 1;
-
-    while (startIndex <= endIndex) {
-      let middleIndex = Math.floor((startIndex + endIndex) / 2);
-
-      if (searchVal == arr[middleIndex]) {
-        return middleIndex;
-      }
-
-      if (searchVal < arr[middleIndex]) {
-        endIndex = middleIndex - 1;
-      }
-
-      if (searchVal > arr[middleIndex]) {
-        startIndex = middleIndex + 1;
-      }
-    }
-  };
+  let n = nums.length;
 
   const swap = (arr, i, j) => {
     const tmp = arr[i];
@@ -80,8 +15,16 @@ var removeElement = function (nums, val) {
     arr[j] = tmp;
   };
 
-  console.log(nums);
-  return nums;
+  for (let i = 0; i < n; i++) {
+    if (nums[nums.length - 1] == val) {
+      nums.pop();
+    }
+    if (nums[i] == val) {
+      swap(nums, i, nums.length - 1);
+      nums.pop();
+    }
+  }
+  return nums, val;
 };
 
 removeElement(nums, val);
