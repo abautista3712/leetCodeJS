ebaae2d6b2ae1768b410d40ec990d56d1e38abbd
Abelard Bautista <abelardbautista@gmail.com>
2021-01-22 00:53:11 -0800

    While loop with splice to retain order.

--- Arrays 101/removeDuplicatesFromSortedArray/2.js
+++ Arrays 101/removeDuplicatesFromSortedArray/2.js
@@ -2,25 +2,16 @@
 //  * @param {number[]} nums
 //  * @return {number}
 //  */
-const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
 var removeDuplicates = function (nums) {
-  const swap = (nums, i, j) => {
-    const tmp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = tmp;
-  };
-
   let i = nums.length - 1;
   while (i >= 0) {
     if (nums[i] == nums[i - 1]) {
-      swap(nums, i, nums.length - 1);
-      nums.pop();
+      nums.splice(i - 1, 1);
     }
     i--;
   }
-  let n = nums.length;
-  return n;
 };
 
 removeDuplicates(nums);
