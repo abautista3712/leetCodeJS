650e24ed0e691a428409478284bc629957fc648f
Abelard Bautista <abelardbautista@gmail.com>
2021-01-21 23:26:20 -0800

    Decrementing while loop with swap and pop.

--- Arrays 101/removeElement/2.js
+++ Arrays 101/removeElement/2.js
@@ -3,28 +3,25 @@
 //  * @param {number} val
 //  * @return {number}
 //  */
-const nums = [0, 1, 2, 2, 3, 0, 4, 2];
-const val = 2;
+const nums = [3, 1, 3, 3, 3];
+const val = 3;
 
 var removeElement = function (nums, val) {
-  let n = nums.length;
-
   const swap = (arr, i, j) => {
     const tmp = arr[i];
     arr[i] = arr[j];
     arr[j] = tmp;
   };
 
-  for (let i = 0; i < n; i++) {
-    if (nums[nums.length - 1] == val) {
-      nums.pop();
-    }
+  let i = nums.length - 1;
+  while (i >= 0) {
     if (nums[i] == val) {
       swap(nums, i, nums.length - 1);
       nums.pop();
     }
+    i--;
   }
-  return nums, val;
 };
 
 removeElement(nums, val);
+console.log(nums, val);
