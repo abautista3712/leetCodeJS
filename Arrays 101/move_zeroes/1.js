3a91d53d2a811f863d4274a6b3ed51df915895b4
Abelard Bautista <abelardbautista@gmail.com>
2021-01-26 00:24:44 -0800

    Decrementing while loop. Splice at i. Push 0.

---
+++ Arrays 101/move_zeroes/1.js
@@ -0,0 +1,20 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+
+const nums = [0, 1, 0, 3, 12];
+
+var moveZeroes = function (nums) {
+  let i = nums.length - 1;
+  while (i >= 0) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
+    i--;
+  }
+};
+
+moveZeroes(nums);
+console.log(nums);
