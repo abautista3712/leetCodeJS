eb2f5ecffecee5723670838d10adcd76ac288bac
Abelard Bautista <abelardbautista@gmail.com>
2021-01-28 23:25:10 -0800

    Fill with even numbers first.

--- Arrays 101/sort_array_by_parity/1.js
+++ Arrays 101/sort_array_by_parity/1.js
@@ -2,4 +2,21 @@
 //  * @param {number[]} A
 //  * @return {number[]}
 //  */
-var sortArrayByParity = function (A) {};
+const A = [0, 1, 0, 3, 12];
+var sortArrayByParity = function (A) {
+  const n = A.length;
+  let index = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (A[i] % 2 == 0) {
+      A[index++] = A[i];
+    }
+  }
+  //   while (index < A.length) {
+  //     A[index++] = 0;
+  //   }
+
+  return A;
+};
+
+console.log(sortArrayByParity(A));
