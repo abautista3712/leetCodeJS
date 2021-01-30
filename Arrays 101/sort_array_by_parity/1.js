54f9d911e4cfac9f5e5adb20d9edb60784507304
Abelard Bautista <abelardbautista@gmail.com>
2021-01-29 23:35:13 -0800

    Approach with decrementing loop.

--- Arrays 101/sort_array_by_parity/1.js
+++ Arrays 101/sort_array_by_parity/1.js
@@ -5,20 +5,14 @@
 const A = [3, 1, 2, 4];
 var sortArrayByParity = function (A) {
   const n = A.length;
-  let index = 0;
 
   if (!A || n == 0) {
     return [];
   }
 
-  for (let i = 0; i < n; i++) {
-    if (A[i] % 2 == 0) {
-      A[index++] = A[i];
-    }
+  for (let i = n - 1, index = A[n]; i >= 0; i--) {
+    console.log(i);
   }
-  //   while (index < A.length) {
-  //     A[index++] = 0;
-  //   }
 
   return A;
 };
