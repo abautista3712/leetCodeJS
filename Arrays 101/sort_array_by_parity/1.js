a7275177db0774b718a24474ae6ed2d75f3ddc0d
Abelard Bautista <abelardbautista@gmail.com>
2021-01-30 23:04:37 -0800

    Two successive loops with space complexity O(N).

--- Arrays 101/sort_array_by_parity/1.js
+++ Arrays 101/sort_array_by_parity/1.js
@@ -5,16 +5,25 @@
 const A = [3, 1, 2, 4];
 var sortArrayByParity = function (A) {
   const n = A.length;
+  let parityArr = [];
 
   if (!A || n == 0) {
     return [];
   }
 
-  for (let i = n - 1, index = A[n]; i >= 0; i--) {
-    console.log(i);
+  for (let i = 0; i < n; i++) {
+    if (A[i] % 2 == 0) {
+      parityArr.push(A[i]);
+    }
   }
 
-  return A;
+  for (let j = 0; j < n; j++) {
+    if (A[j] % 2 != 0) {
+      parityArr.push(A[j]);
+    }
+  }
+
+  return parityArr;
 };
 
 console.log(sortArrayByParity(A));
