f02b9a6c09d3f3885e29f3cbba343537a543b128
Abelard Bautista <abelardbautista@gmail.com>
2021-01-31 23:08:11 -0800

    Started new try.

---
+++ Arrays 101/removeElement/3.js
@@ -0,0 +1,12 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} val
+//  * @return {number}
+//  */
+const nums = [3, 1, 3, 3, 3];
+const val = 3;
+
+var removeElement = function (nums, val) {};
+
+removeElement(nums, val);
+console.log(nums, val);
