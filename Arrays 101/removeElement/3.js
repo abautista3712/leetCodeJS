b871a31b0a3fd3461aa06667a2a720929704dec6
Abelard Bautista <abelardbautista@gmail.com>
2021-02-01 23:21:41 -0800

    Two sequential loops. Order then pop.

--- Arrays 101/removeElement/3.js
+++ Arrays 101/removeElement/3.js
@@ -3,10 +3,25 @@
 //  * @param {number} val
 //  * @return {number}
 //  */
-const nums = [3, 1, 3, 3, 3];
-const val = 3;
+const nums = [0, 1, 2, 2, 3, 0, 4, 2];
+const val = 2;
 
-var removeElement = function (nums, val) {};
+var removeElement = function (nums, val) {
+  let n = nums.length;
+  let count = 0;
+
+  for (let i = 0, j = 0; j < n; j++) {
+    if (nums[j] != val) {
+      let tmp = nums[i];
+      nums[i++] = nums[j];
+      nums[j] = tmp;
+      count++;
+    }
+  }
+  while (nums.length > count) {
+    nums.pop();
+  }
+};
 
 removeElement(nums, val);
 console.log(nums, val);
