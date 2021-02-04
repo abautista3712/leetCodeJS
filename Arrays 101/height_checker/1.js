c7f2798f71ca5a05b48cf297d5dae812fb3ce5fa
Abelard Bautista <abelardbautista@gmail.com>
2021-02-04 01:33:53 -0800

    Sort by insertion sort.

--- Arrays 101/height_checker/1.js
+++ Arrays 101/height_checker/1.js
@@ -2,4 +2,23 @@
  * @param {number[]} heights
  * @return {number}
  */
-var heightChecker = function (heights) {};
+const heights = [1, 1, 4, 2, 1, 3];
+
+var heightChecker = function (heights) {
+  const n = heights.length;
+
+  for (let i = 1; i < n; i++) {
+    let currentVal = heights[i];
+    let j = i - 1;
+    while (j >= 0 && heights[j] > currentVal) {
+      heights[j + 1] = heights[j];
+      j--;
+    }
+    heights[j + 1] = currentVal;
+  }
+  return heights;
+  // console.log(heights);
+};
+
+console.log(heightChecker(heights));
+// console.log(heights);
