22227d41b49aae26c72a1d4dad66a155bacca4e9
Abelard Bautista <abelardbautista@gmail.com>
2021-02-06 21:00:18 -0800

    Provided solution for edge cases.

--- Arrays 101/third_maximum_number/1.js
+++ Arrays 101/third_maximum_number/1.js
@@ -2,4 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var thirdMax = function (nums) {};
+const nums = [1, 3];
+
+var thirdMax = function (nums) {
+  const n = nums.length;
+
+  if (!nums || n === 0) {
+    return;
+  }
+
+  if (n < 3) {
+    if (nums[0] > nums[1]) {
+      return nums[0];
+    } else {
+      return nums[1];
+    }
+  }
+};
+
+console.log(thirdMax(nums));
