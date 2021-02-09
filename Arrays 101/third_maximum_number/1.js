11bbc1cb3d6eacdb228690ac4f2fac253016deb7
Abelard Bautista <abelardbautista@gmail.com>
2021-02-08 22:53:35 -0800

    Rearranged to solve edge cases.

--- Arrays 101/third_maximum_number/1.js
+++ Arrays 101/third_maximum_number/1.js
@@ -2,28 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-const nums = [3, 2, 1, 4, 1, 10];
+const nums = [1, 1, 2];
 
 var thirdMax = function (nums) {
-  const n = nums.length;
-
-  if (!nums || n === 0) {
+  if (!nums || nums.length === 0) {
     return;
   }
-  if (n < 2) {
+
+  let res = new Set([...nums].sort((a, b) => b - a));
+  nums = [...res];
+
+  if (nums.length < 2) {
     return nums[0];
   }
 
-  if (n < 3) {
+  if (nums.length < 3) {
     if (nums[0] > nums[1]) {
       return nums[0];
     } else {
       return nums[1];
     }
   }
-
-  let res = new Set([...nums].sort((a, b) => b - a));
-  nums = [...res];
 
   return nums[2];
 };
