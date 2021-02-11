1ebda1c148a692eb98dbfbb27553c25033004396
Abelard Bautista <abelardbautista@gmail.com>
2021-02-10 22:06:20 -0800

    Solution with slice and sort. Comparison and count.

--- Arrays 101/height_checker/1.js
+++ Arrays 101/height_checker/1.js
@@ -5,30 +5,16 @@
 const heights = [1, 1, 4, 2, 1, 3];
 
 var heightChecker = function (heights) {
-  const n = heights.length;
+  let sortedHeights = heights.slice().sort((a, b) => a - b);
+  let count = 0;
 
-  function countingSort(arr, min, max) {
-    var i,
-      z = 0,
-      count = [];
-
-    for (i = min; i <= max; i++) {
-      count[i] = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] != sortedHeights[i]) {
+      count++;
     }
-
-    for (i = 0; i < arr.length; i++) {
-      count[arr[i]]++;
-    }
-
-    for (i = min; i <= max; i++) {
-      while (count[i]-- > 0) {
-        arr[z++] = i;
-      }
-    }
-    return arr;
   }
-  console.log(countingSort(heights, 0, n));
+
+  return count;
 };
 
 console.log(heightChecker(heights));
-// console.log(heights);
