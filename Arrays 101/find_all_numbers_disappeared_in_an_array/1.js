17e7e51134872b2cc9581611703241614f18bc39
Abelard Bautista <abelardbautista@gmail.com>
2021-02-12 22:52:17 -0800

    Pseudocoded approach.

--- Arrays 101/find_all_numbers_disappeared_in_an_array/1.js
+++ Arrays 101/find_all_numbers_disappeared_in_an_array/1.js
@@ -2,4 +2,15 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDisappearedNumbers = function (nums) {};
+
+const nums = [4, 3, 2, 7, 8, 2, 3, 1];
+
+var findDisappearedNumbers = function (nums) {
+  //   Loop through array
+  // Compare number with index
+  // Remove number if equal
+  // Write index number if not equal
+  // Return array
+};
+
+console.log(findDisappearedNumbers(nums));
