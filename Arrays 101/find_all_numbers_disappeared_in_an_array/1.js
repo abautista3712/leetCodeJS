a947907cd6d3556b1042bb59e827ed3f28f4c286
Abelard Bautista <abelardbautista@gmail.com>
2021-02-14 00:07:20 -0800

    Remove all non-inserted elements.

--- Arrays 101/find_all_numbers_disappeared_in_an_array/1.js
+++ Arrays 101/find_all_numbers_disappeared_in_an_array/1.js
@@ -8,10 +8,21 @@ const nums = [4, 3, 2, 7, 8, 2, 3, 1];
 var findDisappearedNumbers = function (nums) {
   let index = 0;
 
-  let uniqueSet = new Set([...nums]);
-  nums = [...uniqueSet];
+  //   let uniqueSet = new Set([...nums]);
+  //   nums = [...uniqueSet];
 
-  for (let i = 0; i < nums.length; i++) {}
+  //   console.log(nums);
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i != nums[i] && nums[i] <= nums.length) {
+    } else {
+      nums[index++] = i;
+    }
+  }
+
+  while (nums.length > index) {
+    nums.pop();
+  }
 
   return nums;
 };
