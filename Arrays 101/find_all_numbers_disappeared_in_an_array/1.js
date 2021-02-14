99a6f0db46525215ff69b41f9a950785039148b2
Abelard Bautista <abelardbautista@gmail.com>
2021-02-13 23:58:23 -0800

    Create unique array of elements using Set.

--- Arrays 101/find_all_numbers_disappeared_in_an_array/1.js
+++ Arrays 101/find_all_numbers_disappeared_in_an_array/1.js
@@ -8,15 +8,10 @@ const nums = [4, 3, 2, 7, 8, 2, 3, 1];
 var findDisappearedNumbers = function (nums) {
   let index = 0;
 
-  //   let uniqueSet = new Set([...nums]);
-  //   nums = [...uniqueSet];
+  let uniqueSet = new Set([...nums]);
+  nums = [...uniqueSet];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === i && nums[i] <= nums.length) {
-    } else {
-      nums[index++] = i;
-    }
-  }
+  for (let i = 0; i < nums.length; i++) {}
 
   return nums;
 };
