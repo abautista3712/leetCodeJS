151fb19de36804d5f954d156570dca989358dffb
Abelard Bautista <abelardbautista@gmail.com>
2021-02-16 18:57:25 -0800

    One while loop, two pointers.

--- Arrays 101/squaresOfASortedArray/7.js
+++ Arrays 101/squaresOfASortedArray/7.js
@@ -3,17 +3,26 @@
  * @return {number[]}
  */
 
-const nums = [-4, -1, 0, 3, 10];
+const nums = [-7, -3, 2, 3, 11];
 
 var sortedSquares = function (nums) {
-  let index = 0;
+  let res = [];
 
-  for (num of nums) {
-    nums[index++] = num ** 2;
+  let left_index = 0,
+    right_index = nums.length - 1;
+
+  while (left_index <= right_index) {
+    let num1 = nums[left_index] ** 2,
+      num2 = nums[right_index] ** 2;
+    if (num1 > num2) {
+      res.push(num1);
+      left_index++;
+    } else {
+      res.push(num2);
+      right_index--;
+    }
   }
-  nums.sort((a, b) => a - b);
-
-  return nums;
+  return res.reverse();
 };
 
 console.log(sortedSquares(nums));
