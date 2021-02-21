b067ac67280ca58d86fd37a34fe44b8c50c9ba8e
Abelard Bautista <abelardbautista@gmail.com>
2021-02-20 23:11:59 -0800

    Solution for example 1.

--- Arrays 101/best_time_to_buy_and_sell_stock_ii/1.js
+++ Arrays 101/best_time_to_buy_and_sell_stock_ii/1.js
@@ -6,18 +6,58 @@
 const prices = [7, 1, 5, 3, 6, 4];
 
 var maxProfit = function (prices) {
-  // Create variable for min (buy price) = 0
-  // Create variable for max value (sell price)
-  // Create variable for total profit
-  // Loop through prices (start index at 1, since dealing with days)
-  //   If min = 0, assign first number as min
-  //   If prices[i] < min, reassign min
-  //   Find max value or first value that satisfies either:
-  //     prices[i+1] < prices[i]
-  //     OR
-  //     end of array
-  // Subtract max from min in total profit variable, reset min and max
-  // Repeat until end of array
+  const n = prices.length;
+
+  // Create variables for:
+  // min (buy price)
+  // max (sell price)
+  // total_profit
+  let min = 0;
+  let max = 0;
+  let total_profit = 0;
+
+  // Loop through prices
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+
+    console.log(`min = ${min}`);
+    console.log(`prices[min] = ${prices[min]}`);
+
+    console.log(`prices[i] = ${prices[i]}`);
+
+    // if (min === 0) {
+    //   console.log("min === 0");
+    //   console.log("assigning min...");
+    //   min = i;
+    //   console.log(`min = ${min}`);
+    //   console.log(`prices[min] = ${prices[min]}`);
+    // }
+
+    //   If prices[i] < min, reassign min
+    if (prices[i] < prices[min]) {
+      console.log("prices[i] < prices[min]...");
+      min = i;
+    } else {
+      // If prices[i] > min, assign max
+      console.log("assigning max...");
+      max = i;
+      console.log(`max = ${max}`);
+      console.log(`prices[max] = ${prices[max]}`);
+    }
+
+    // Once max consecutive value has been reached:
+    // Calculate profit by subracting max from min
+    // Reset max and min variables
+    if (max != 0) {
+      if (i === n || prices[max + 1] < prices[max]) {
+        console.log("calculating profit...");
+        total_profit = total_profit + (prices[max] - prices[min]);
+        min = 0;
+        max = 0;
+      }
+    }
+  }
+  return total_profit;
 };
 
 console.log(maxProfit(prices));
