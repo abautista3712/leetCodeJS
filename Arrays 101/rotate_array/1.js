8199971d7249f1db368aefae9ecd32b4e48b4814
Abelard Bautista <abelardbautista@gmail.com>
2021-02-23 01:39:55 -0800

    Solution with array cache using modulo operator.

--- Arrays 101/rotate_array/1.js
+++ Arrays 101/rotate_array/1.js
@@ -3,4 +3,20 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {};
+
+const nums = [-1, -100, 3, 99];
+const k = 2;
+
+var rotate = function (nums, k) {
+  let new_index = 0;
+
+  let nums_cache = [...nums];
+
+  for (let i = 0; i < nums.length; i++) {
+    new_index = (i + k) % nums.length;
+    nums_cache[new_index] = nums[i];
+  }
+  return nums_cache;
+};
+
+console.log(rotate(nums, k));
