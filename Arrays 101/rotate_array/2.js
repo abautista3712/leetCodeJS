e9dce3b9926696754e06e564380a99059d6bef0a
Abelard Bautista <abelardbautista@gmail.com>
2021-02-24 17:51:42 -0800

    Reverse function solution with three calls.

--- Arrays 101/rotate_array/2.js
+++ Arrays 101/rotate_array/2.js
@@ -5,12 +5,34 @@
  */
 
 const nums = [1, 2, 3, 4, 5, 6, 7];
-const k = 3;
+const k = 1;
 
 var rotate = function (nums, k) {
   if (nums == null || nums.length < 2) {
     return;
   }
+
+  let reverse = (nums, i, j) => {
+    let tmp = 0;
+    while (i < j) {
+      tmp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = tmp;
+      i++;
+      j--;
+    }
+  };
+
+  console.log(nums);
+
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - k - 1);
+  console.log(nums);
+  reverse(nums, nums.length - k, nums.length - 1);
+  console.log(nums);
+  reverse(nums, 0, nums.length - 1);
+
+  return nums;
 };
 
 console.log(rotate(nums, k));
