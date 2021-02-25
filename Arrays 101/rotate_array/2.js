ca59c7bfc82a89842fcdedcc39bb7cbdb136dd0d
Abelard Bautista <abelardbautista@gmail.com>
2021-02-24 17:14:45 -0800

    Started new approach. Added edge cases.

---
+++ Arrays 101/rotate_array/2.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+const nums = [1, 2, 3, 4, 5, 6, 7];
+const k = 3;
+
+var rotate = function (nums, k) {
+  if (nums == null || nums.length < 2) {
+    return;
+  }
+};
+
+console.log(rotate(nums, k));
