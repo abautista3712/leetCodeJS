ce57f41780fc77b3965df71256699c4410a69318
Abelard Bautista <abelardbautista@gmail.com>
2021-02-26 21:14:09 -0800

    Added beginning code over template.

--- Arrays 101/contains_duplicate/1.js
+++ Arrays 101/contains_duplicate/1.js
@@ -1,22 +1,9 @@
 /**
  * @param {number[]} nums
- * @param {number} k
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @return {boolean}
  */
+const nums = [1, 2, 3, 1];
 
-const nums = [-1, -100, 3, 99];
-const k = 2;
+var containsDuplicate = function (nums) {};
 
-var rotate = function (nums, k) {
-  let new_index = 0;
-
-  let nums_cache = [...nums];
-
-  for (let i = 0; i < nums.length; i++) {
-    new_index = (i + k) % nums.length;
-    nums[new_index] = nums_cache[i];
-  }
-  return nums;
-};
-
-console.log(rotate(nums, k));
+console.log(containsDuplicate(nums));
