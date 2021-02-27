f165603f81c6074ac56a4c2a2d06c2adeb06c301
Abelard Bautista <abelardbautista@gmail.com>
2021-02-26 22:22:45 -0800

    Nested loop solution.

--- Arrays 101/contains_duplicate/1.js
+++ Arrays 101/contains_duplicate/1.js
@@ -2,8 +2,19 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-const nums = [1, 2, 3, 1];
+const nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
 
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 
 console.log(containsDuplicate(nums));
