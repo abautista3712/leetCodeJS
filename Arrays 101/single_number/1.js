7f174d43355b510a791f4d7ef90a426a9624807a
Abelard Bautista <abelardbautista@gmail.com>
2021-02-28 18:21:01 -0800

    Sort then conditional.

--- Arrays 101/single_number/1.js
+++ Arrays 101/single_number/1.js
@@ -2,8 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-const nums = [2, 2, 1];
+const nums = [1];
 
-var singleNumber = function (nums) {};
+var singleNumber = function (nums) {
+  const n = nums.length;
+
+  let double = false;
+
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === nums[i + 1]) {
+      double = true;
+    }
+    if (nums[i] != nums[i + 1]) {
+      if (double === true) {
+        double = false;
+      } else return nums[i];
+    }
+  }
+  return "All numbers appear twice.";
+};
 
 console.log(singleNumber(nums));
