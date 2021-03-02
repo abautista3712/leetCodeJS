a1315d6e4cb926f37ea9beefc0cc50c365970ffd
Abelard Bautista <abelardbautista@gmail.com>
2021-03-01 20:53:24 -0800

    Conditionally add one to last digit if value is not 9. While loop to handle if value is 9.

--- Arrays 101/plus_one/1.js
+++ Arrays 101/plus_one/1.js
@@ -2,4 +2,22 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function (digits) {};
+
+const digits = [1, 9, 9];
+
+var plusOne = function (digits) {
+  let n = digits.length - 1;
+  if (digits[n] != 9) {
+    digits[n] = digits[n] + 1;
+  } else {
+    digits[n] = 0;
+    while (digits[n - 1] === 9) {
+      digits[n - 1] = 0;
+      n--;
+    }
+    digits[n - 1] = digits[n - 1] + 1;
+  }
+  return digits;
+};
+
+console.log(plusOne(digits));
