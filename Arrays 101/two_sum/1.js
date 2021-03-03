0a563ff15f6241f86853b4f62041befcf54988dd
Abelard Bautista <abelardbautista@gmail.com>
2021-03-02 18:43:40 -0800

    While loop subtracts values from target. Pop unused elements. Only viable for some solutions.

--- Arrays 101/two_sum/1.js
+++ Arrays 101/two_sum/1.js
@@ -3,4 +3,25 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+
+const nums = [3, 2, 4];
+let target = 6;
+
+var twoSum = function (nums, target) {
+  let count = 0;
+  let i = 0;
+
+  while (target > 0) {
+    if (nums[i] <= target) {
+      target -= nums[i];
+      nums[count++] = i;
+    }
+    i++;
+  }
+  while (nums.length > count) {
+    nums.pop();
+  }
+  return nums;
+};
+
+console.log(twoSum(nums, target));
