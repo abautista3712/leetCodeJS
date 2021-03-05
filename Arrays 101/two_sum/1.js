40de64cb30c6385cd77d266f2526991b091a8ba4
Abelard Bautista <abelardbautista@gmail.com>
2021-03-05 00:01:51 -0800

    Added solution values <= target.

--- Arrays 101/two_sum/1.js
+++ Arrays 101/two_sum/1.js
@@ -4,17 +4,31 @@
  * @return {number[]}
  */
 
-const nums = [11, 15, 2, 7];
-let target = 9;
+const nums = [3, 3];
+let target = 6;
 
 var twoSum = function (nums, target) {
   let total = 0;
+  let count = 0;
   let indexes = [];
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] <= target) {
       total += nums[i];
       indexes.push(i);
+
+      if (total > target) {
+        total -= nums[count];
+
+        let tmp = indexes[count];
+        indexes[count] = indexes[indexes.length - 1];
+        indexes[indexes.length - 1] = tmp;
+
+        count++;
+
+        indexes.pop();
+      }
+
       if (total === target) {
         return indexes;
       }
