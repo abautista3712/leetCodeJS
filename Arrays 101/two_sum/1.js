3318c3497bb16fd1f898f5521168961270716625
Abelard Bautista <abelardbautista@gmail.com>
2021-03-05 22:50:55 -0800

    Nested for loop subtracting difference.

--- Arrays 101/two_sum/1.js
+++ Arrays 101/two_sum/1.js
@@ -4,32 +4,41 @@
  * @return {number[]}
  */
 
-const nums = [3, 3];
+const nums = [3, 2, 3];
 let target = 6;
 
 var twoSum = function (nums, target) {
   let total = 0;
+  let subtract = 0;
   let count = 0;
   let indexes = [];
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] <= target) {
       total += nums[i];
+      count++;
       indexes.push(i);
 
       if (total > target) {
-        total -= nums[count];
+        subtract = total - target;
 
-        let tmp = indexes[count];
-        indexes[count] = indexes[indexes.length - 1];
-        indexes[indexes.length - 1] = tmp;
+        for (let j = 0; j < nums.length; j++) {
+          if (nums[j] === subtract) {
+            total -= subtract;
 
-        count++;
+            let tmp = indexes[indexes.length - 1];
+            indexes[indexes.length - 1] = indexes[j];
+            indexes[j] = tmp;
 
-        indexes.pop();
+            count--;
+          }
+        }
       }
 
       if (total === target) {
+        while (indexes.length > count) {
+          indexes.pop();
+        }
         return indexes;
       }
     }
