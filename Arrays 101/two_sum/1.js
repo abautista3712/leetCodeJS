33755fcbeb0040e2cc2da4fecd4c5ee934a35428
Abelard Bautista <abelardbautista@gmail.com>
2021-03-07 19:37:35 -0800

    New approach using hashmap.

--- Arrays 101/two_sum/1.js
+++ Arrays 101/two_sum/1.js
@@ -4,51 +4,21 @@
  * @return {number[]}
  */
 
-const nums = [0, 7, 11, 0];
+const nums = [-3, 7, 11, 3];
 let target = 0;
 
 var twoSum = function (nums, target) {
-  let total = 0;
-  let subtract = 0;
-  let count = 0;
-  let indexes = [];
-
+  let result = [];
+  let map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    if (target === 0 && nums[i] === 0) {
-      indexes.push(i);
-
-      if (i === nums.length - 1) {
-        return indexes;
-      }
-    } else if (nums[i] <= target) {
-      total += nums[i];
-      count++;
-      indexes.push(i);
-
-      if (total > target) {
-        subtract = total - target;
-
-        for (let j = 0; j < nums.length; j++) {
-          if (nums[j] === subtract) {
-            total -= subtract;
-
-            let tmp = indexes[indexes.length - 1];
-            indexes[indexes.length - 1] = indexes[j];
-            indexes[j] = tmp;
-
-            count--;
-          }
-        }
-      }
-
-      if (total === target) {
-        while (indexes.length > count) {
-          indexes.pop();
-        }
-        return indexes;
-      }
+    if (map.has(target - nums[i])) {
+      result[1] = i;
+      result[0] = map.get(target - nums[i]);
+      return result;
     }
+    map.set(nums[i], i);
   }
+  return result;
 };
 
 console.log(twoSum(nums, target));
