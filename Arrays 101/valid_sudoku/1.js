899c350a0703924a69aa0a4fefd675b65790e2f0
Abelard Bautista <abelardbautista@gmail.com>
2021-03-08 19:03:38 -0800

    Approach with hash table.

--- Arrays 101/valid_sudoku/1.js
+++ Arrays 101/valid_sudoku/1.js
@@ -2,4 +2,38 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {};
+
+const board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+];
+
+var isValidSudoku = function (board) {
+  // Create Hash Table
+  let hash = [];
+  for (let i = 0; i < 9; i++) {
+    hash.push(0);
+  }
+
+  // Check Row
+  for (let i = 0; i < 9; i++) {
+    if (board[0][i] == ".") {
+      i++;
+    } else if (hash[parseInt([board[0][i]]) - 1] == 1) {
+      return false;
+    } else {
+      hash[parseInt([board[0][i]]) - 1]++;
+    }
+  }
+
+  console.log(hash);
+};
+
+isValidSudoku(board);
