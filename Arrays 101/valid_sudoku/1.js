f52430b6a733b6e6d4449582ac900557a8be55de
Abelard Bautista <abelardbautista@gmail.com>
2021-03-13 22:35:46 -0800

    Added solution to sub-box.

--- Arrays 101/valid_sudoku/1.js
+++ Arrays 101/valid_sudoku/1.js
@@ -23,48 +23,35 @@ var isValidSudoku = function (board) {
 
     for (let j = 0; j < 9; j++) {
       // // Check Row
-      // if (board[i][j] != ".") {
-      //   let number = parseInt(board[i][j] - 1);
-      //   if (hash_row[number]) {
-      //     return false;
-      //   }
-      //   hash_row[number] = number + 1;
-      // }
+      if (board[i][j] != ".") {
+        let number = parseInt(board[i][j] - 1);
+        if (hash_row[number]) {
+          return false;
+        }
+        hash_row[number] = number + 1;
+      }
 
       // // Check Column
-      // if (board[j][i] != ".") {
-      //   let number = parseInt(board[j][i] - 1);
-      //   if (hash_column[number]) {
-      //     return false;
-      //   }
-      //   hash_column[number] = number + 1;
-      // }
+      if (board[j][i] != ".") {
+        let number = parseInt(board[j][i] - 1);
+        if (hash_column[number]) {
+          return false;
+        }
+        hash_column[number] = number + 1;
+      }
 
       // Check Sub-Box
-      let m = (i / 3) * 3 + j / 3;
-      let n = (j / 3) * 3 + (j % 3);
+      let m = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+      let n = ((i * 3) % 9) + (j % 3);
 
-      console.log(`i = ${i}`);
-      console.log(m);
-      console.log(n);
-
-      // 0,0   0,1   0,2
-
-      // 1,0   1,1   1,2
-
-      // 2,0   2,1   2,2
-
-      // console.log(board);
-
-      // if (board[m][n] != ".") {
-      //   let number = parseInt(board[m][n] - 1);
-      //   if (hash_sub_box[number]) {
-      //     return false;
-      //   }
-      //   hash_sub_box[number] = number + 1;
-      // }
+      if (board[m][n] != ".") {
+        let number = parseInt(board[m][n] - 1);
+        if (hash_sub_box[number]) {
+          return false;
+        }
+        hash_sub_box[number] = number + 1;
+      }
     }
-    console.log(hash_row);
   }
 };
 
