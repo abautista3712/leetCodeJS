ab89b83fe750590250c3ba931c602200c77fc059
Abelard Bautista <abelardbautista@gmail.com>
2021-03-18 23:47:26 -0700

    Solution using JS built-in function.

--- Strings/reverse_string/1.js
+++ Strings/reverse_string/1.js
@@ -2,4 +2,12 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function (s) {};
+
+const s = ["h", "e", "l", "l", "o"];
+
+var reverseString = function (s) {
+  s.reverse();
+  console.log(s);
+};
+
+reverseString(s);
