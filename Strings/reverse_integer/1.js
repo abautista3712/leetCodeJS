0fe62f7a6110d33cdac11d3e03ca14ed3469e84a
Abelard Bautista <abelardbautista@gmail.com>
2021-03-21 01:58:22 -0700

    Added solution for x < 0.

--- Strings/reverse_integer/1.js
+++ Strings/reverse_integer/1.js
@@ -3,15 +3,20 @@
  * @return {number}
  */
 
-const x = 120;
+const x = -123;
 
 var reverse = function (x) {
   let buffer = [];
   let last_digit = 0;
+  let is_negative = false;
 
   if (x == 0) {
     return x;
+  } else if (x < 0) {
+    is_negative = true;
   }
+
+  x = Math.abs(x);
 
   while (x > 0) {
     last_digit = x % 10;
@@ -20,6 +25,9 @@ var reverse = function (x) {
   }
 
   x = parseInt(buffer.join(""));
+  if (is_negative == true) {
+    x = -x;
+  }
   return x;
 };
 
