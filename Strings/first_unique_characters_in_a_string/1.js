256470a817fba70b02461d06edd7ffa639e9ab53
Abelard Bautista <abelardbautista@gmail.com>
2021-03-21 02:25:28 -0700

    Pseudocoded approach.

--- Strings/first_unique_characters_in_a_string/1.js
+++ Strings/first_unique_characters_in_a_string/1.js
@@ -2,4 +2,14 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function (s) {};
+
+const s = "leetcode";
+
+var firstUniqChar = function (s) {
+  // Hash Table
+  // Read each letter
+  // Register new letters / Increment old letters
+  // Return index of letter that only shows once
+};
+
+console.log(firstUniqChar(s));
