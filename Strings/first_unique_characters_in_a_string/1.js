b8e16d78d5b09a0eb806286f918d85bd07245404
Abelard Bautista <abelardbautista@gmail.com>
2021-03-22 22:25:13 -0700

    Iterate through letters in a string using charAt.

--- Strings/first_unique_characters_in_a_string/1.js
+++ Strings/first_unique_characters_in_a_string/1.js
@@ -7,6 +7,15 @@ const s = "leetcode";
 
 var firstUniqChar = function (s) {
   // Hash Table
+  let hash_table = {};
+
+  for (let i = 0; i < s.length; i++) {
+    console.log(s.charAt(i));
+  }
+  // let letter = s.charAt(0);
+  // hash_table. = "Test";
+  // console.log(hash_table);
+  // console.log(s.charCodeAt(2));
   // Read each letter
   // Register new letters / Increment old letters
   // Return index of letter that only shows once
