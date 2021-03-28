62a61df4a80c5df8bc64a49209360da0f9b145df
Abelard Bautista <abelardbautista@gmail.com>
2021-03-28 13:04:06 -0700

    Added hash_table for string t.

--- Strings/valid_anagram/1.js
+++ Strings/valid_anagram/1.js
@@ -8,21 +8,36 @@ const s = "anagram";
 const t = "nagaram";
 
 var isAnagram = function (s, t) {
-  let hash_table = {};
+  let hash_table_s = {};
+  let hash_table_t = {};
 
   for (let i = 0; i < s.length; i++) {
-    let key = s.charCodeAt(i);
+    let key_s = s.charCodeAt(i);
+    let key_t = t.charCodeAt(i);
 
-    console.log(key);
-    if (!hash_table[key]) {
-      count = 1;
-    } else if (hash_table[key] >= 1) {
-      count++;
+    console.log(key_s);
+    if (!hash_table_s[key_s]) {
+      count_s = 1;
+    } else if (hash_table_s[key_s] >= 1) {
+      count_s++;
     }
-    hash_table[key] = count;
+    hash_table_s[key_s] = count_s;
 
-    console.log(hash_table);
+    console.log(key_t);
+    if (!hash_table_t[key_t]) {
+      count_t = 1;
+    } else if (hash_table_t[key_t] >= 1) {
+      count_t++;
+    }
+    hash_table_t[key_t] = count_t;
+
+    console.log(hash_table_s);
+    console.log(hash_table_t);
   }
+
+  //   if (hash_table_s === hash_table_t) {
+  //     return "Test";
+  //   }
 };
 
 isAnagram(s, t);
