cb6395f690c5a0324fab81dde01ed054cd84c8af
Abelard Bautista <abelardbautista@gmail.com>
2021-03-28 13:27:49 -0700

    Solution using ES6 JS built-in functions.

--- Arrays/intersection_of_two_arrays_ii/1.js
+++ Arrays/intersection_of_two_arrays_ii/1.js
@@ -3,4 +3,12 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function (nums1, nums2) {};
+const nums1 = [4, 9, 5];
+const nums2 = [9, 4, 9, 8, 4];
+
+var intersect = function (nums1, nums2) {
+  let filteredArray = nums1.filter((x) => nums2.includes(x));
+  console.log(filteredArray);
+};
+
+console.log(intersect(nums1, nums2));
