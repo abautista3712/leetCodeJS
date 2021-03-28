6aa2acc3e4b9a2f8bfcb8377152f61509bf30f25
Abelard Bautista <abelardbautista@gmail.com>
2021-03-28 12:53:24 -0700

    Count instances of letter in string using hash_table.

--- Strings/valid_anagram/1.js
+++ Strings/valid_anagram/1.js
@@ -3,4 +3,26 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function (s, t) {};
+
+const s = "anagram";
+const t = "nagaram";
+
+var isAnagram = function (s, t) {
+  let hash_table = {};
+
+  for (let i = 0; i < s.length; i++) {
+    let key = s.charCodeAt(i);
+
+    console.log(key);
+    if (!hash_table[key]) {
+      count = 1;
+    } else if (hash_table[key] >= 1) {
+      count++;
+    }
+    hash_table[key] = count;
+
+    console.log(hash_table);
+  }
+};
+
+isAnagram(s, t);
