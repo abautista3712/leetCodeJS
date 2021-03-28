44c0cd969067ede78423384dac4ae666b466e2ce
Abelard Bautista <abelardbautista@gmail.com>
2021-03-28 13:09:57 -0700

    JSON.stringify both objects and compare.

--- Strings/valid_anagram/1.js
+++ Strings/valid_anagram/1.js
@@ -15,7 +15,7 @@ var isAnagram = function (s, t) {
     let key_s = s.charCodeAt(i);
     let key_t = t.charCodeAt(i);
 
-    console.log(key_s);
+    // console.log(key_s);
     if (!hash_table_s[key_s]) {
       count_s = 1;
     } else if (hash_table_s[key_s] >= 1) {
@@ -23,7 +23,7 @@ var isAnagram = function (s, t) {
     }
     hash_table_s[key_s] = count_s;
 
-    console.log(key_t);
+    // console.log(key_t);
     if (!hash_table_t[key_t]) {
       count_t = 1;
     } else if (hash_table_t[key_t] >= 1) {
@@ -31,13 +31,18 @@ var isAnagram = function (s, t) {
     }
     hash_table_t[key_t] = count_t;
 
-    console.log(hash_table_s);
-    console.log(hash_table_t);
+    // console.log(hash_table_s);
+    // console.log(hash_table_t);
   }
 
-  //   if (hash_table_s === hash_table_t) {
-  //     return "Test";
-  //   }
+  let s_string = JSON.stringify(hash_table_s);
+  let t_string = JSON.stringify(hash_table_t);
+
+  if (s_string == t_string) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-isAnagram(s, t);
+console.log(isAnagram(s, t));
