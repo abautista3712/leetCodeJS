3cf34c59ecd448b7ec3458f505fe236a71d73e54
Abelard Bautista <abelardbautista@gmail.com>
2021-03-28 21:36:58 -0700

    Count is reset using value in hash then incremented.

--- Strings/valid_anagram/1.js
+++ Strings/valid_anagram/1.js
@@ -4,8 +4,8 @@
  * @return {boolean}
  */
 
-const s = "a";
-const t = "ab";
+const s = "dgqztusjuu";
+const t = "dqugjzutsu";
 
 var isAnagram = function (s, t) {
   if (s.length != t.length) {
@@ -19,24 +19,21 @@ var isAnagram = function (s, t) {
     let key_s = s.charCodeAt(i);
     let key_t = t.charCodeAt(i);
 
-    // console.log(key_s);
     if (!hash_table_s[key_s]) {
       count_s = 1;
     } else if (hash_table_s[key_s] >= 1) {
+      count_s = hash_table_s[key_s];
       count_s++;
     }
     hash_table_s[key_s] = count_s;
 
-    // console.log(key_t);
     if (!hash_table_t[key_t]) {
       count_t = 1;
     } else if (hash_table_t[key_t] >= 1) {
+      count_t = hash_table_t[key_t];
       count_t++;
     }
     hash_table_t[key_t] = count_t;
-
-    console.log(hash_table_s);
-    console.log(hash_table_t);
   }
 
   let s_string = JSON.stringify(hash_table_s);
