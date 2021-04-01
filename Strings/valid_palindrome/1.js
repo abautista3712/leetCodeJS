a9af7bcecb0a760a5ee9e553b2c273106ecef5e1
Abelard Bautista <abelardbautista@gmail.com>
2021-03-31 23:42:10 -0700

    Isolate only uppercase and lowercase letters.

--- Strings/valid_palindrome/1.js
+++ Strings/valid_palindrome/1.js
@@ -2,4 +2,20 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function (s) {};
+
+const s = "A man, a plan, a canal: Panama";
+
+var isPalindrome = function (s) {
+  // console.log(s.charCodeAt(1));
+  // console.log(s);
+  for (let i = 0; i < s.length; i++) {
+    if (
+      (s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90) ||
+      (s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122)
+    ) {
+      console.log(s.charAt(i));
+    }
+  }
+};
+
+console.log(isPalindrome(s));
