223def220fddbc000c858068b86bcc416017f68d
Abelard Bautista <abelardbautista@gmail.com>
2021-04-01 23:03:20 -0700

    Convert uppercase to lower.

--- Strings/valid_palindrome/1.js
+++ Strings/valid_palindrome/1.js
@@ -6,16 +6,18 @@
 const s = "A man, a plan, a canal: Panama";
 
 var isPalindrome = function (s) {
-  // console.log(s.charCodeAt(1));
-  // console.log(s);
+  let letters = [];
+
   for (let i = 0; i < s.length; i++) {
-    if (
-      (s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90) ||
-      (s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122)
-    ) {
-      console.log(s.charAt(i));
+    let lower_case = String.fromCharCode(s.charCodeAt(i) + 32);
+
+    if (s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90) {
+      letters.push(lower_case);
+    } else if (s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122) {
+      letters.push(s.charAt(i));
     }
   }
+  console.log(letters);
 };
 
 console.log(isPalindrome(s));
