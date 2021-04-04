2600101189c34c8bd7897939521e61eb9fa1e012
Abelard Bautista <abelardbautista@gmail.com>
2021-04-03 23:22:22 -0700

    Fixed is_alpha_numeric function.

--- Strings/valid_palindrome/1.js
+++ Strings/valid_palindrome/1.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 
-const s = "A man, a plan, a canal: Panama";
+const s = "      A man, a plan, a canal: Panama";
 
 var isPalindrome = function (s) {
   // for (let i = 0; i < s.length; i++) {
@@ -11,9 +11,9 @@ var isPalindrome = function (s) {
   // Function to check if alpha-numeric
   const is_alpha_numeric = (char) => {
     if (
-      (s.charCodeAt(char) >= 48 && s.charCodeAt(char) <= 57) ||
-      (s.charCodeAt(char) >= 65 && s.charCodeAt(char) <= 90) ||
-      (s.charCodeAt(char) >= 97 && s.charCodeAt(char) <= 122)
+      (char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57) ||
+      (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) ||
+      (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)
     ) {
       return true;
     }
@@ -32,20 +32,33 @@ var isPalindrome = function (s) {
   };
   // }
 
-  // let i = 0;
-  // let j = s.length - 1;
+  let i = 0;
+  let j = s.length - 1;
 
-  let char = s.charAt(0);
-  // while (i < j) {
-  if (is_alpha_numeric(char)) {
-    if (s.charCodeAt(char) >= 65 && s.charCodeAt(char) <= 90) {
-      return make_lowercase(char);
-    } else {
-      return char;
-    }
-  } else {
-    console.log("Character is NOT alpha-numeric");
-  }
+  let forward_char = s.charAt(i);
+  let reverse_char = s.charAt(j);
+
+  return is_alpha_numeric("A");
+
+  // while (i <= j) {
+  // if (is_alpha_numeric(s.charAt(i)) == false) {
+  //   ("Test");
+  // }
+  // if (is_alpha_numeric(forward_char)) {
+  //   if (
+  //     s.charCodeAt(forward_char) >= 65 &&
+  //     s.charCodeAt(forward_char) <= 90
+  //   ) {
+  //     return make_lowercase(forward_char);
+  //   } else {
+  //     return forward_char;
+  //   }
+  // } else {
+  //   console.log("Character is NOT alpha-numeric");
+  // }
+  // console.log(forward_char);
+  // }
+  // return console.log(forward_char);
   // if (characters[i] == characters[j]) {
   //     i++;
   //     j--;
