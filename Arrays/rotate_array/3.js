a90f7588effd583f842340189ce8e1a2b495f583
Abelard Bautista <abelardbautista@gmail.com>
2021-04-08 22:23:37 -0700

    Started new third approach.

---
+++ Arrays/rotate_array/3.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+const nums = [1, 2, 3, 4, 5, 6, 7];
+const k = 3;
+
+var rotate = function (nums, k) {};
+
+console.log(rotate(nums, k));
