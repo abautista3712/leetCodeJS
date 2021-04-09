2920810258a075f134380ba1578ed3bb8fdf0bd7
Abelard Bautista <abelardbautista@gmail.com>
2021-04-08 23:29:46 -0700

    Solution for particular case.

--- Arrays/rotate_array/3.js
+++ Arrays/rotate_array/3.js
@@ -7,6 +7,26 @@
 const nums = [1, 2, 3, 4, 5, 6, 7];
 const k = 3;
 
-var rotate = function (nums, k) {};
+var rotate = function (nums, k) {
+  let cache = [];
+  let count = k;
+
+  for (let i = k + 1; i < nums.length; i++) {
+    cache.push(nums[i]);
+  }
+
+  let reverse_cache = cache.reverse();
+  let rev_count = reverse_cache.length - 1;
+
+  for (let j = nums.length - 1; j >= 0; j--) {
+    if (j >= k) {
+      nums[j] = nums[count--];
+    } else {
+      nums[j] = reverse_cache[rev_count--];
+    }
+  }
+
+  return nums;
+};
 
 console.log(rotate(nums, k));
