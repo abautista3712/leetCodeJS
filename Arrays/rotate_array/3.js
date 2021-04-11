e2fd942cdbe9dfc84c41e17796da3b7e8e6f3206
Abelard Bautista <abelardbautista@gmail.com>
2021-04-10 19:14:31 -0700

    Extended solution to incorporate k values above nums.length.

--- Arrays/rotate_array/3.js
+++ Arrays/rotate_array/3.js
@@ -4,28 +4,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-const nums = [1, 2, 3, 4, 5, 6, 7];
-const k = 1;
+const nums = [1, 2];
+const k = 3;
 
 var rotate = function (nums, k) {
   let cache = [];
   let count = 0;
 
+  k = k % nums.length;
+
   for (let i = nums.length - 1; i >= 0; i--) {
-    console.log(`---i = ${i}---`);
     if (i + k > nums.length - 1) {
       cache.push(nums[i]);
     }
 
     nums[i] = nums[i - k];
 
-    console.log(cache);
-
     if (nums[i - k] == undefined) {
       nums[i] = cache[count++];
     }
-
-    console.log(nums);
   }
   return nums;
 };
