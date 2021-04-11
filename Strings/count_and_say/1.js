dd2120aab538fad79533a081734be82166450142
Abelard Bautista <abelardbautista@gmail.com>
2021-04-11 00:36:20 -0700

    Added conditional to concatenate strings.

--- Strings/count_and_say/1.js
+++ Strings/count_and_say/1.js
@@ -2,14 +2,30 @@
  * @param {number} n
  * @return {string}
  */
-const n = 1;
+const n = 5;
 
 var countAndSay = function (n) {
-  let value = n;
+  let concat_value = "";
+  let value = JSON.stringify(11121);
+  n = value.length;
   let how_many = 1;
-  if (value == 1 && how_many == 1) {
+
+  if (n == 1) {
     return 1;
   }
+
+  for (let i = 0; i < n; i++) {
+    if (value[i] == value[i + 1]) {
+      how_many++;
+    } else {
+      concat_value = JSON.stringify(how_many).concat("", value[i]);
+      return concat_value;
+    }
+  }
+
+  // return concat_value;
+
+  // let i = 0;
 };
 
 console.log(countAndSay(n));
