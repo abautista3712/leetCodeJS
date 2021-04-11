9b9a7a62a822587850d7a6ea729dff2f7ebe0c52
Abelard Bautista <abelardbautista@gmail.com>
2021-04-10 23:03:25 -0700

    Reverse sub-arrays.

--- Arrays/rotate_image/1.js
+++ Arrays/rotate_image/1.js
@@ -9,6 +9,12 @@ const matrix = [
   [7, 8, 9],
 ];
 
-var rotate = function (matrix) {};
+var rotate = function (matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i] = matrix[i].reverse(...matrix[i]);
+  }
+
+  return matrix;
+};
 
 console.log(rotate(matrix));
