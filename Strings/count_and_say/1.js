db081c6b33e5b39d7bcf85633bcab069da4ecfe5
Abelard Bautista <abelardbautista@gmail.com>
2021-04-10 23:46:26 -0700

    Solution for base case.

--- Strings/count_and_say/1.js
+++ Strings/count_and_say/1.js
@@ -2,4 +2,14 @@
  * @param {number} n
  * @return {string}
  */
-var countAndSay = function (n) {};
+const n = 1;
+
+var countAndSay = function (n) {
+  let value = n;
+  let how_many = 1;
+  if (value == 1 && how_many == 1) {
+    return 1;
+  }
+};
+
+console.log(countAndSay(n));
