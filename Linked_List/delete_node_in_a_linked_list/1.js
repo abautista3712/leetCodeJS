128d11136d3a33e92c460dbce9c85195d27d19bb
Abelard Bautista <abelardbautista@gmail.com>
2021-04-11 19:05:32 -0700

    Remove element at index.

--- Linked_List/delete_node_in_a_linked_list/1.js
+++ Linked_List/delete_node_in_a_linked_list/1.js
@@ -9,4 +9,20 @@
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
-var deleteNode = function (node) {};
+
+const head = [4, 5, 1, 9];
+const node = 5;
+
+var deleteNode = function (node) {
+  let node_index = 0;
+
+  for (let i = 0; i < head.length; i++) {
+    if (head[i] == node) {
+      node_index = i;
+    }
+  }
+  head.splice(1, node_index);
+  return head;
+};
+
+console.log(deleteNode(node));
