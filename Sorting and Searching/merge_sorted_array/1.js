9bb16e2320b75f68ea54223f271a20b0fb245dca
Abelard Bautista <abelardbautista@gmail.com>
2021-04-14 20:44:33 -0700

    Started new approach.

--- Sorting and Searching/merge_sorted_array/1.js
+++ Sorting and Searching/merge_sorted_array/1.js
@@ -5,4 +5,30 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {};
+
+const nums1 = [1, 5, 6, 0, 0, 0];
+const m = 3;
+const nums2 = [2, 3, 4];
+const n = 3;
+
+var merge = function (nums1, m, nums2, n) {
+  let i = 0;
+  let j = 0;
+
+  while (i < m + n) {
+    if (nums1[i] < nums2[j]) {
+      i++;
+    } else if (nums1[i] > nums2[j]) {
+      let tmp = nums1[i];
+      nums1[i] = nums2[j];
+      nums2[j] = tmp;
+    }
+    if (i > m) {
+      nums1[i] = nums2[j];
+      j++;
+    }
+  }
+  return nums1;
+};
+
+console.log(merge(nums1, m, nums2, n));
