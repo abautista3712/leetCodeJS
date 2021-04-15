27ed4f11ce4e9922fb36f0b15d1f38e95f9569e5
Abelard Bautista <abelardbautista@gmail.com>
2021-04-14 23:23:52 -0700

    Solution via binary search.

--- Sorting and Searching/first_bad_version/1.js
+++ Sorting and Searching/first_bad_version/1.js
@@ -17,5 +17,19 @@ var solution = function (isBadVersion) {
    * @param {integer} n Total versions
    * @return {integer} The first bad version
    */
-  return function (n) {};
+  return function (n) {
+    let left = 0;
+    let right = n;
+
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return right;
+  };
 };
