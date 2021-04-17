44c0be6d16cf90f5cd60783726e980f6e853ea61
Abelard Bautista <abelardbautista@gmail.com>
2021-04-16 17:33:53 -0700

    Added implementation of factorial function.

--- Dynamic Programming/climbing_stairs/1.js
+++ Dynamic Programming/climbing_stairs/1.js
@@ -8,6 +8,21 @@ const n = 2;
 var climbStairs = function (n) {
   // Permutation w/o repetition
   // (n!) / (n - r)!
+
+  const factorial = (num) => {
+    if (num == 0) {
+      return 1;
+    } else {
+      let total = 1;
+      while (num > 0) {
+        total = total * num;
+        num--;
+      }
+      // console.log(total);
+      return total;
+    }
+  };
+  factorial(4);
 };
 
 console.log(climbStairs(n));
