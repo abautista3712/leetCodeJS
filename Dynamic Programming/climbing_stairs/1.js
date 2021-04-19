b575c0cdaf7ca006c86ca8d65e0b30f772029a19
Abelard Bautista <abelardbautista@gmail.com>
2021-04-18 20:15:09 -0700

    Added solution for n == 2.

--- Dynamic Programming/climbing_stairs/1.js
+++ Dynamic Programming/climbing_stairs/1.js
@@ -6,12 +6,11 @@
 const n = 3;
 
 var climbStairs = function (n) {
+  if (n == 1 || n == 2) {
+    return n;
+  }
   // Fibonacci Sequence
   // S(n) = S(n-1) + S(n-2)
-
-  if (n == 1) {
-    return 1;
-  }
 };
 
 console.log(climbStairs(n));
