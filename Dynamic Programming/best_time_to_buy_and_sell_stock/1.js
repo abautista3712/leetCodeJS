0b61dd54f8f81eafe7e0543d314f1a5c9110e39c
Abelard Bautista <abelardbautista@gmail.com>
2021-04-20 15:54:45 -0700

    Added code to find min value.

--- Dynamic Programming/best_time_to_buy_and_sell_stock/1.js
+++ Dynamic Programming/best_time_to_buy_and_sell_stock/1.js
@@ -2,4 +2,21 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {};
+
+const prices = [7, 1, 5, 3, 6, 4];
+
+var maxProfit = function (prices) {
+  let i = 0;
+  let j = prices.length - 1;
+
+  while (j >= 0) {
+    if (!min) {
+      let min = prices[j];
+    } else {
+      min = prices[j];
+    }
+    j--;
+  }
+};
+
+console.log(maxProfit(prices));
