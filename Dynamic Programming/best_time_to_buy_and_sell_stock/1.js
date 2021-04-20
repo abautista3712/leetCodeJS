6d6a6a168f196dc9ae6236d490cad39be9601233
Abelard Bautista <abelardbautista@gmail.com>
2021-04-20 16:12:41 -0700

    Added loop to find max valude.

--- Dynamic Programming/best_time_to_buy_and_sell_stock/1.js
+++ Dynamic Programming/best_time_to_buy_and_sell_stock/1.js
@@ -9,7 +9,17 @@ var maxProfit = function (prices) {
   let i = 0;
   let j = prices.length - 1;
 
-  let min;
+  let min, max;
+
+  while (i < prices.length) {
+    if (!max) {
+      max = prices[i];
+    } else if (max < prices[i]) {
+      max = prices[i];
+    }
+    i++;
+  }
+  return max;
 
   while (j >= 0) {
     if (!min) {
