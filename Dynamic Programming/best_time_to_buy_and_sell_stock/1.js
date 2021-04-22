34000ba926e4d45929b9f1d2cf5553214fbe472b
Abelard Bautista <abelardbautista@gmail.com>
2021-04-21 17:14:12 -0700

    Restarted new approach.

--- Dynamic Programming/best_time_to_buy_and_sell_stock/1.js
+++ Dynamic Programming/best_time_to_buy_and_sell_stock/1.js
@@ -7,36 +7,12 @@ const prices = [3, 2, 6, 5, 0, 3];
 
 var maxProfit = function (prices) {
   let i = 0;
-  let j = prices.length - 1;
 
   let min, max;
 
   if (!prices.length || prices.length == 1) {
     return 0;
   }
-
-  while (i <= j) {
-    if (!min) {
-      min = prices[i];
-    } else if (min > prices[i]) {
-      min = prices[i];
-    }
-
-    if (!max) {
-      max = prices[j];
-    } else if (max < prices[j]) {
-      max = prices[j];
-    }
-    i++;
-    j--;
-  }
-
-  console.log(`min = ${min}`);
-  console.log(`max = ${max}`);
-
-  if (min <= max) {
-    return max - min;
-  } else return 0;
 };
 
 console.log(maxProfit(prices));
