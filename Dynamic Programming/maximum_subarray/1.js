08b97496d8b6c715c58013e7ec646d88eacb9934
Abelard Bautista <abelardbautista@gmail.com>
2021-04-25 03:18:22 -0700

    Started new problem.

---
+++ Dynamic Programming/maximum_subarray/1.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+var maxSubArray = function (nums) {};
+
+console.log(maxSubArray(nums));
