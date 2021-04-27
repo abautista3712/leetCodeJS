fd6ccaadb132836d8e22c8e487e665fd37695725
Abelard Bautista <abelardbautista@gmail.com>
2021-04-26 22:13:02 -0700

    Started new approach using Kadane's Algorithm.

--- Dynamic Programming/maximum_subarray/1.js
+++ Dynamic Programming/maximum_subarray/1.js
@@ -5,6 +5,13 @@
 
 const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-var maxSubArray = function (nums) {};
+var maxSubArray = function (nums) {
+  // Kadane's Algorithm
+  let local_max = -Infinity;
+  let global_max = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {}
+  return global_max;
+};
 
 console.log(maxSubArray(nums));
