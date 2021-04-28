2580f2c886e4a92079108c412a607754344e72bc
Abelard Bautista <abelardbautista@gmail.com>
2021-04-27 23:27:49 -0700

    Modified implementation to include incrementing sum.

--- Dynamic Programming/maximum_subarray/1.js
+++ Dynamic Programming/maximum_subarray/1.js
@@ -7,16 +7,17 @@ const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
 var maxSubArray = function (nums) {
   // Kadane's Algorithm
-  let local_max = -Infinity;
-  let global_max = nums[0];
+  let sum = 0;
+  let max_sum = -Infinity;
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    local_max = Math.max(nums[i], (local_max = nums[i] + nums[i + 1]));
-    global_max = Math.max(local_max, global_max);
-    console.log(`local_max = ${local_max}`);
-    console.log(`global_max = ${global_max}`);
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    max_sum = Math.max(sum, max_sum);
+    if (sum < 0) {
+      sum = 0;
+    }
   }
-  return global_max;
+  return max_sum;
 };
 
 console.log(maxSubArray(nums));
