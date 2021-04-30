e95acdc868934fbeea0b342cdb00e8e5fefb7cd7
Abelard Bautista <abelardbautista@gmail.com>
2021-04-29 21:24:06 -0700

    Sum odds and separately sum evens. Return max.

--- Dynamic Programming/house_robber/1.js
+++ Dynamic Programming/house_robber/1.js
@@ -2,4 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function (nums) {};
+
+const nums = [2, 7, 9, 3, 1];
+
+var rob = function (nums) {
+  let odds = 0;
+  let evens = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      odds += nums[i];
+    } else {
+      evens += nums[i];
+    }
+  }
+  return Math.max(evens, odds);
+};
+
+console.log(rob(nums));
