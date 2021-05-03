e3b8b7ce36a22634eb181539ea5eacc85e480f5c
Abelard Bautista <abelardbautista@gmail.com>
2021-05-02 17:40:22 -0700

    Added function to handle finding if number is prime or not.

--- Math/count_primes/1.js
+++ Math/count_primes/1.js
@@ -8,6 +8,15 @@ const n = 10;
 var countPrimes = function (n) {
   let count = 0;
 
+  const isPrime = (num) => {
+    for (let i = 2; i < num - 1; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
   if (n === 1) {
     return 0;
   } else if (n > 1) {
