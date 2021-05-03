bcb1c6b5e8c298c10a6a47eb71081ac8cedcb955
Abelard Bautista <abelardbautista@gmail.com>
2021-05-02 17:25:47 -0700

    Added solution if n === 1.

--- Math/count_primes/1.js
+++ Math/count_primes/1.js
@@ -2,4 +2,14 @@
  * @param {number} n
  * @return {number}
  */
-var countPrimes = function (n) {};
+
+const n = 10;
+
+var countPrimes = function (n) {
+  if (n === 1) {
+    return 0;
+  } else if (n > 1) {
+  }
+};
+
+console.log(countPrimes(n));
