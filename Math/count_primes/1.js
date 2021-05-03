291fc03676fa00ee0c3ed45623e0764941cc096c
Abelard Bautista <abelardbautista@gmail.com>
2021-05-02 17:28:53 -0700

    Added structure for solution.

--- Math/count_primes/1.js
+++ Math/count_primes/1.js
@@ -6,10 +6,19 @@
 const n = 10;
 
 var countPrimes = function (n) {
+  let count = 0;
+
   if (n === 1) {
     return 0;
   } else if (n > 1) {
+    for (let i = 2; i < n; i++) {
+      if (isPrime(i)) {
+        count++;
+      }
+    }
   }
+
+  return count;
 };
 
 console.log(countPrimes(n));
