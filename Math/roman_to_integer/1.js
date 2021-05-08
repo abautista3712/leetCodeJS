20f8556ade56c562ea66f5b0855cbbc577d36af3
Abelard Bautista <abelardbautista@gmail.com>
2021-05-07 19:55:13 -0700

    Return value.

--- Math/roman_to_integer/1.js
+++ Math/roman_to_integer/1.js
@@ -9,19 +9,19 @@ var romanToInt = function (s) {
   let total = 0;
 
   if (s.charAt(0) == "I") {
-    total += 1;
+    return (total += 1);
   } else if (s.charAt(0) == "V") {
-    total += 5;
+    return (total += 5);
   } else if (s.charAt(0) == "X") {
-    total += 10;
+    return (total += 10);
   } else if (s.charAt(0) == "L") {
-    total += 50;
+    return (total += 50);
   } else if (s.charAt(0) == "C") {
-    total += 100;
+    return (total += 100);
   } else if (s.charAt(0) == "D") {
-    total += 500;
+    return (total += 500);
   } else if (s.charAt(0) == "M") {
-    total += 1000;
+    return (total += 1000);
   }
 };
 
