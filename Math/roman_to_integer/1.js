8e4f26f0233a2c45412c7a8b784f5cfe83e3c107
Abelard Bautista <abelardbautista@gmail.com>
2021-05-07 19:59:35 -0700

    Changed s.charAt(0) to s.charAt(i).

--- Math/roman_to_integer/1.js
+++ Math/roman_to_integer/1.js
@@ -9,19 +9,19 @@ var romanToInt = function (s) {
   let total = 0;
 
   for (let i = 0; i < s.length; i++) {
-    if (s.charAt(0) == "I") {
+    if (s.charAt(i) == "I") {
       total += 1;
-    } else if (s.charAt(0) == "V") {
+    } else if (s.charAt(i) == "V") {
       total += 5;
-    } else if (s.charAt(0) == "X") {
+    } else if (s.charAt(i) == "X") {
       total += 10;
-    } else if (s.charAt(0) == "L") {
+    } else if (s.charAt(i) == "L") {
       total += 50;
-    } else if (s.charAt(0) == "C") {
+    } else if (s.charAt(i) == "C") {
       total += 100;
-    } else if (s.charAt(0) == "D") {
+    } else if (s.charAt(i) == "D") {
       total += 500;
-    } else if (s.charAt(0) == "M") {
+    } else if (s.charAt(i) == "M") {
       total += 1000;
     }
   }
