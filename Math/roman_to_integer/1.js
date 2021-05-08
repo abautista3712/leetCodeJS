184b46d388b19619660a6a3ec6563ca9f4b7638b
Abelard Bautista <abelardbautista@gmail.com>
2021-05-07 19:57:34 -0700

    Read entire string.

--- Math/roman_to_integer/1.js
+++ Math/roman_to_integer/1.js
@@ -8,21 +8,24 @@ const s = "III";
 var romanToInt = function (s) {
   let total = 0;
 
-  if (s.charAt(0) == "I") {
-    return (total += 1);
-  } else if (s.charAt(0) == "V") {
-    return (total += 5);
-  } else if (s.charAt(0) == "X") {
-    return (total += 10);
-  } else if (s.charAt(0) == "L") {
-    return (total += 50);
-  } else if (s.charAt(0) == "C") {
-    return (total += 100);
-  } else if (s.charAt(0) == "D") {
-    return (total += 500);
-  } else if (s.charAt(0) == "M") {
-    return (total += 1000);
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(0) == "I") {
+      total += 1;
+    } else if (s.charAt(0) == "V") {
+      total += 5;
+    } else if (s.charAt(0) == "X") {
+      total += 10;
+    } else if (s.charAt(0) == "L") {
+      total += 50;
+    } else if (s.charAt(0) == "C") {
+      total += 100;
+    } else if (s.charAt(0) == "D") {
+      total += 500;
+    } else if (s.charAt(0) == "M") {
+      total += 1000;
+    }
   }
+  return total;
 };
 
 console.log(romanToInt(s));
