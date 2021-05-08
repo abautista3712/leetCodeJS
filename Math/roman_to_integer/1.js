4a7ddccc9475573869f6dbe1382005b61144437f
Abelard Bautista <abelardbautista@gmail.com>
2021-05-07 20:02:55 -0700

    Added subtraction exceptions for I, X, C roman numerals.

--- Math/roman_to_integer/1.js
+++ Math/roman_to_integer/1.js
@@ -3,22 +3,34 @@
  * @return {number}
  */
 
-const s = "III";
+const s = "XL";
 
 var romanToInt = function (s) {
   let total = 0;
 
   for (let i = 0; i < s.length; i++) {
     if (s.charAt(i) == "I") {
-      total += 1;
+      if (s.charAt(i + 1) == "V" || s.charAt(i + 1) == "X") {
+        total -= 1;
+      } else {
+        total += 1;
+      }
     } else if (s.charAt(i) == "V") {
       total += 5;
     } else if (s.charAt(i) == "X") {
-      total += 10;
+      if (s.charAt(i + 1) == "L" || s.charAt(i + 1) == "C") {
+        total -= 10;
+      } else {
+        total += 10;
+      }
     } else if (s.charAt(i) == "L") {
       total += 50;
     } else if (s.charAt(i) == "C") {
-      total += 100;
+      if (s.charAt(i + 1) == "D" || s.charAt(i + 1) == "M") {
+        total -= 100;
+      } else {
+        total += 100;
+      }
     } else if (s.charAt(i) == "D") {
       total += 500;
     } else if (s.charAt(i) == "M") {
