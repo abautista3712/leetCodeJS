1090c5d00b374821678f2c68fb0fc01c81337e76
Abelard Bautista <abelardbautista@gmail.com>
2021-05-07 19:54:43 -0700

    Encoded conversion between roman to integer.

--- Math/roman_to_integer/1.js
+++ Math/roman_to_integer/1.js
@@ -9,7 +9,19 @@ var romanToInt = function (s) {
   let total = 0;
 
   if (s.charAt(0) == "I") {
-    return 1;
+    total += 1;
+  } else if (s.charAt(0) == "V") {
+    total += 5;
+  } else if (s.charAt(0) == "X") {
+    total += 10;
+  } else if (s.charAt(0) == "L") {
+    total += 50;
+  } else if (s.charAt(0) == "C") {
+    total += 100;
+  } else if (s.charAt(0) == "D") {
+    total += 500;
+  } else if (s.charAt(0) == "M") {
+    total += 1000;
   }
 };
 
