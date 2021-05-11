2e8b17eeae1ee2e05ff43717732acab8e3045e85
Abelard Bautista <abelardbautista@gmail.com>
2021-05-10 22:21:25 -0700

    Added switch statement to handle conversion from string to array value.

--- Math/roman_to_integer/2.js
+++ Math/roman_to_integer/2.js
@@ -9,7 +9,32 @@ var romanToInt = function (s) {
   let int_s = [];
 
   for (let i = 0; i < s.length; i++) {
-    int_s[i] = s.charAt(i);
+    switch (s.charAt(0)) {
+      case "I":
+        int_s[i] = 1;
+        break;
+      case "V":
+        int_s[i] = 5;
+        break;
+      case "X":
+        int_s[i] = 10;
+        break;
+      case "L":
+        int_s[i] = 50;
+        break;
+      case "C":
+        int_s[i] = 100;
+        break;
+      case "D":
+        int_s[i] = 500;
+        break;
+      case "M":
+        int_s[i] = 1000;
+        break;
+      default:
+        "Character is not a valid roman numeral";
+        break;
+    }
   }
   return int_s;
 };
