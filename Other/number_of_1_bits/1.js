a0e9c1d83764a501531ee7f8dda1a8b9070e720a
Abelard Bautista <abelardbautista@gmail.com>
2021-05-19 21:12:26 -0700

    Count number of 1's using split(1).length - 1.

--- Other/number_of_1_bits/1.js
+++ Other/number_of_1_bits/1.js
@@ -6,7 +6,9 @@ const n = 3;
 
 var hammingWeight = function (n) {
   let result = n.toString(2);
-  return result;
+  let count = result.split(1).length - 1;
+
+  return count;
 };
 
 console.log(hammingWeight(n));
