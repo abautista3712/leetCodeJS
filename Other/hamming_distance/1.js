0a35d8de990c2abee870d01c1bab6a7956c92966
Abelard Bautista <abelardbautista@gmail.com>
2021-05-21 20:50:04 -0700

    Added pseudocode to map approach.

--- Other/hamming_distance/1.js
+++ Other/hamming_distance/1.js
@@ -3,4 +3,18 @@
  * @param {number} y
  * @return {number}
  */
-var hammingDistance = function (x, y) {};
+const x = 1;
+const y = 4;
+
+var hammingDistance = function (x, y) {
+  // Convert integer x to binary
+  // Convert integer y to binary
+  // Conditional statement
+  //     -Scan from right to left
+  //     -Boolean variable, default false
+  //         -If 1, set to true
+  //         -Move left, add one to Count variable
+  //         -If 1 again, return Count variable
+};
+
+console.log(hammingDistance(x, y));
