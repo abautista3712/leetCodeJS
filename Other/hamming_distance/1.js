da42ceaf379d7f1aa7fdca4b88648a0bf1d09c93
Abelard Bautista <abelardbautista@gmail.com>
2021-05-26 21:41:20 -0700

    Debugging case when x=4, y=14.

--- Other/hamming_distance/1.js
+++ Other/hamming_distance/1.js
@@ -3,15 +3,18 @@
  * @param {number} y
  * @return {number}
  */
-const x = 93;
-const y = 73;
+const x = 4;
+const y = 14;
 
 var hammingDistance = function (x, y) {
   const bi_x = x.toString(2);
   const bi_y = y.toString(2);
 
+  console.log(bi_x);
+  console.log(bi_y);
+
   if (bi_x.length != bi_y.length) {
-    return Math.abs(bi_x.length - bi_y.length);
+    let length_diff = Math.abs(bi_x.length - bi_y.length);
   } else {
     let n = bi_x.length;
     let is_binary_equal = true;
