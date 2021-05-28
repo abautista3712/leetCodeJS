dd057aa1b2fb35ef9c6a44c40b4acb3472a1d880
Abelard Bautista <abelardbautista@gmail.com>
2021-05-27 22:34:49 -0700

    Solution using JS built-in functions .includes and .indexOf.

--- Strings/implement_strStr()/1.js
+++ Strings/implement_strStr()/1.js
@@ -4,28 +4,12 @@
  * @return {number}
  */
 
-const haystack = "hello";
-const needle = "ll";
+const haystack = "";
+const needle = "";
 
 var strStr = function (haystack, needle) {
-  let hash_table = {};
-
-  for (let i = 0; i < haystack.length; i++) {
-    hash_table.haystack = [];
-    hash_table.needle = [];
-  }
-  // let haystack_arr = [];
-  // let needle_arr = [];
-
-  // for (let i = 0; i < haystack.length; i++) {
-  //   haystack_arr.push(haystack.charAt(i));
-  // }
-  // for (let j = 0; j < needle.length; j++) {
-  //   needle_arr.push(needle.charAt(j));
-  // }
-
-  // let filteredArr = haystack_arr.filter((x) => needle_arr.includes(x));
-  // return filteredArr;
+  haystack.includes(needle) ? (x = haystack.indexOf(needle)) : (x = -1);
+  return x;
 };
 
 console.log(strStr(haystack, needle));
