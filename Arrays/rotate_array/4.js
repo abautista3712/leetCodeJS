54671e4c52fcd65192d0cc6477becb9dd53d87df
Abelard Bautista <abelardbautista@gmail.com>
2021-05-28 21:03:03 -0700

    Started new approach. Added high-level pseudocode.

---
+++ Arrays/rotate_array/4.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+const nums = [1, 2];
+const k = 3;
+
+var rotate = function (nums, k) {
+  // Reverse on diagonal
+  // Reverse order
+};
+
+console.log(rotate(nums, k));
