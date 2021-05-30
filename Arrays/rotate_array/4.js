7be9a9ce11ad2c4f3df7470963fd81996b72f435
Abelard Bautista <abelardbautista@gmail.com>
2021-05-29 22:44:59 -0700

    More efficient cache with spread operator.

--- Arrays/rotate_array/4.js
+++ Arrays/rotate_array/4.js
@@ -8,11 +8,11 @@ const nums = [1, 2, 3, 4, 5, 6, 7];
 const k = 3;
 
 var rotate = function (nums, k) {
-  let cache = [];
+  let cache = [...nums];
 
-  for (let i = 0; i < nums.length; i++) {
-    cache.push(nums[i]);
-  }
+  return cache;
+
+  return k % nums.length;
 };
 
 console.log(rotate(nums, k));
