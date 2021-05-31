cfe3530492be313fe583456ddc8c73f37ab55178
Abelard Bautista <abelardbautista@gmail.com>
2021-05-30 19:26:22 -0700

    Started new approach. Added pseudocode.

---
+++ Arrays/rotate_image/2.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+var rotate = function (matrix) {
+  //  Transpose diagonally
+  //  Reverse left and right
+};
+
+console.log(rotate(matrix));
