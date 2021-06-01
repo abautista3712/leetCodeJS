02d5e61c316170b6c56699b7a04697004f7a8daf
Abelard Bautista <abelardbautista@gmail.com>
2021-05-31 19:06:25 -0700

    Added visualization to pseudocode for transposing diagonally.

--- Arrays/rotate_image/2.js
+++ Arrays/rotate_image/2.js
@@ -11,6 +11,12 @@ const matrix = [
 
 var rotate = function (matrix) {
   //  Transpose diagonally
+  // [1,2,3]
+  // [4,5,6]
+  // [7,8,9]
+  // [1,4,7]
+  // [2,5,8]
+  // [3,6,9]
   //  Reverse left and right
 };
 
