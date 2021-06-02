64edf90577e0886c793afade1a3ffce6091c7370
Abelard Bautista <abelardbautista@gmail.com>
2021-06-01 18:44:05 -0700

    Formatted pseudocode for easier viewing.

--- Arrays/rotate_image/2.js
+++ Arrays/rotate_image/2.js
@@ -11,12 +11,9 @@ const matrix = [
 
 var rotate = function (matrix) {
   //  Transpose diagonally
-  // [1,2,3]
-  // [4,5,6]
-  // [7,8,9]
-  // [1,4,7]
-  // [2,5,8]
-  // [3,6,9]
+  // [1,2,3]    [1,4,7]
+  // [4,5,6] -> [2,5,8]
+  // [7,8,9]    [3,6,9]
   //  Reverse left and right
 };
 
