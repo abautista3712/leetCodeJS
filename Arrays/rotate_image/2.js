bf76cde34628b24921e4c44aefac22bc70c4f6fa
Abelard Bautista <abelardbautista@gmail.com>
2021-06-03 23:16:00 -0700

    Elaborated logic in pseudocode.

--- Arrays/rotate_image/2.js
+++ Arrays/rotate_image/2.js
@@ -15,17 +15,22 @@ var rotate = function (matrix) {
   // [4,5,6] -> [2,5,8]
   // [7,8,9]    [3,6,9]
 
+  // (matrix[0][0]) <-> (matrix[0][0]);
   // (matrix[0][1]) <-> (matrix[1][0]);
   // (matrix[0][2]) <-> (matrix[2][0]);
+
+  // (matrix[1][1]) <-> (matrix[1][1]);
   // (matrix[1][2]) <-> (matrix[2][1]);
+
+  // (matrix[2][2]) <-> (matrix[2][2]);
 
   let n = matrix.length;
 
   for (let i = 0; i < n; i++) {
     for (let j = i; j < n; j++) {
-      let tmp = matrix[j][i];
-      matrix[j][i] = matrix[i][j];
-      matrix[i][j] = tmp;
+      let tmp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = tmp;
     }
   }
 
@@ -35,6 +40,15 @@ var rotate = function (matrix) {
   // [1,4,7]      [7,4,1]
   // [2,5,8]  ->  [8,5,2]
   // [3,6,9]      [9,6,3]
+
+  // (matrix[0][0]) <-> (matrix[0][2]);
+  // (matrix[0][1]) <-> (matrix[0][1]);
+
+  // (matrix[1][0]) <-> (matrix[1][2]);
+  // (matrix[1][1]) <-> (matrix[1][1]);
+
+  // (matrix[2][0]) <-> (matrix[2][2]);
+  // (matrix[2][1]) <-> (matrix[2][1]);
 };
 
 console.log(rotate(matrix));
