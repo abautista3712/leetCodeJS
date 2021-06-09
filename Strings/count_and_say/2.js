cfbb1845c77a2b25f22f5ba4f6f611e39d69aa6b
Abelard Bautista <abelardbautista@gmail.com>
2021-06-08 22:57:33 -0700

    Added pseudocode.

--- Strings/count_and_say/2.js
+++ Strings/count_and_say/2.js
@@ -2,8 +2,15 @@
  * @param {number} n
  * @return {string}
  */
-const n = 5;
+const n = 4;
 
-var countAndSay = function (n) {};
+var countAndSay = function (n) {
+  // Read
+  // -Create number variable to keep track of current value being read
+  // Say
+  // -Convert number variable to "say" value
+  // Write
+  // -Convert number to string and return
+};
 
 console.log(countAndSay(n));
