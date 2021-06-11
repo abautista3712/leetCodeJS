6bf8e76399d9ea18f0d9e46b211c9d9eeeec4bef
Abelard Bautista <abelardbautista@gmail.com>
2021-06-11 03:41:14 -0700

    Reorganized base case in loop.

--- Strings/count_and_say/2.js
+++ Strings/count_and_say/2.js
@@ -2,16 +2,16 @@
  * @param {number} n
  * @return {string}
  */
-const n = 4;
+const n = 2;
 
 var countAndSay = function (n) {
-  // Base Case
-  if (n == 1) {
-    return "1";
-  }
-
   // Read
   // -Create number variable to keep track of current value being read
+  for (let i = 0; i < n; i++) {
+    if (n == 1) {
+      return "1";
+    }
+  }
 
   // Say
   // -Convert number variable to "say" value
