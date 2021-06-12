09eeddaa0db57bf6d7a0acae13dd2b6eef040db2
Abelard Bautista <abelardbautista@gmail.com>
2021-06-12 16:18:42 -0700

    Added output_string variable.

--- Strings/count_and_say/2.js
+++ Strings/count_and_say/2.js
@@ -7,9 +7,13 @@ const n = 2;
 var countAndSay = function (n) {
   // Read
   // -Create number variable to keep track of current value being read
-  for (let i = 0; i < n; i++) {
+  let count_value;
+
+  for (let i = 1; i <= n; i++) {
+    let output_string = n.toString;
+
     if (n == 1) {
-      return "1";
+      return output_string;
     }
   }
 
