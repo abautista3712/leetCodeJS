b36f52580171331e1bf60d6701e092d3c2099b52
Abelard Bautista <abelardbautista@gmail.com>
2021-06-12 21:57:20 -0700

    Solution for up to n = 3.

--- Strings/count_and_say/2.js
+++ Strings/count_and_say/2.js
@@ -2,20 +2,17 @@
  * @param {number} n
  * @return {string}
  */
-const n = 2;
+const n = 3;
 
 var countAndSay = function (n) {
-  // Read
-  // -Create number variable to keep track of current value being read
   let output_string = "1";
 
-  for (let i = 1; i <= n; i++) {
-    if (n == 1) {
-      return output_string;
-    }
+  if (n == 1) {
+    return output_string;
+  }
 
+  for (let i = 2; i <= n; i++) {
     let str_length = output_string.length;
-
     let hash = {};
 
     for (let j = 0; j < str_length; j++) {
@@ -34,11 +31,6 @@ var countAndSay = function (n) {
   }
 
   return output_string;
-
-  // Say
-  // -Convert number variable to "say" value
-  // Write
-  // -Convert number to string and return
 };
 
 console.log(countAndSay(n));
