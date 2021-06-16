d7dc7ba9e47cc735f96769d5c8346994efa5503d
Abelard Bautista <abelardbautista@gmail.com>
2021-06-15 22:06:38 -0700

    Added pseudocode.

--- Math/power_of_three/2.js
+++ Math/power_of_three/2.js
@@ -3,5 +3,9 @@
  * @return {boolean}
  */
 const n = 45;
-var isPowerOfThree = function (n) {};
+var isPowerOfThree = function (n) {
+  // Find cube root of number
+  // If number has a remainder, return false
+  // Otherwise (if no remainder), return true
+};
 console.log(isPowerOfThree(n));
