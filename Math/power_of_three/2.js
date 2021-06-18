cd8b395d9371b57ab8b2aeb4600759857e49e5e7
Abelard Bautista <abelardbautista@gmail.com>
2021-06-17 22:26:09 -0700

    Added rounding logic.

--- Math/power_of_three/2.js
+++ Math/power_of_three/2.js
@@ -2,8 +2,12 @@
  * @param {number} n
  * @return {boolean}
  */
-const n = 27;
+const n = 45;
 var isPowerOfThree = function (n) {
-  return Math.log(n) / Math.log(3);
+  if (Math.log(n) % Math.log(3) > 0.00001) {
+    return false;
+  } else {
+    return true;
+  }
 };
 console.log(isPowerOfThree(n));
