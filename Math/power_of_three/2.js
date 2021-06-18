e00086905242d571d9f0b4664410daeb3784a4df
Abelard Bautista <abelardbautista@gmail.com>
2021-06-17 22:32:01 -0700

    Refined rounding logic.

--- Math/power_of_three/2.js
+++ Math/power_of_three/2.js
@@ -2,11 +2,11 @@
  * @param {number} n
  * @return {boolean}
  */
-const n = 45;
+const n = 177148;
 var isPowerOfThree = function (n) {
   if (n == 0) {
     return false;
-  } else if (Math.log(n) % Math.log(3) > 0.00001) {
+  } else if (Math.log(n) % Math.log(3) > 10 ** -15) {
     return false;
   } else {
     return true;
