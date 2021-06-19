4edc3ac5fbbb5b5b16da0934f8d602eeca63ab93
Abelard Bautista <abelardbautista@gmail.com>
2021-06-18 23:45:17 -0700

    Conditional if n > 0.

--- Math/power_of_three/2.js
+++ Math/power_of_three/2.js
@@ -2,16 +2,20 @@
  * @param {number} n
  * @return {boolean}
  */
-const n = 2187;
+const n = -3;
 var isPowerOfThree = function (n) {
   let mod_log3 = Math.log(n) % Math.log(3);
 
   if (n == 0) {
     return false;
-  } else if (Math.abs(mod_log3) > 10 ** -14) {
-    return false;
-  } else {
-    return true;
+  }
+
+  if (n > 0) {
+    if (mod_log3 > 10 ** -14) {
+      return false;
+    } else {
+      return true;
+    }
   }
 };
 console.log(isPowerOfThree(n));
