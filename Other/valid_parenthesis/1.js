57d39aeefd4597949d24b1baa741e22be74a5652
Abelard Bautista <abelardbautista@gmail.com>
2021-06-21 19:26:11 -0700

    Refactored first conditional for easier reading.

--- Other/valid_parenthesis/1.js
+++ Other/valid_parenthesis/1.js
@@ -9,45 +9,51 @@ var isValid = function (s) {
   let orderCache = [];
 
   for (let i = 0; i < s.length; i++) {
-    if ((s.charAt(i) == "(") | (s.charAt(i) == "{") | (s.charAt(i) == "[")) {
-      isOpen = true;
-      orderCache.push(s.charAt(i));
-    } else {
-      let close_bracket = s.charAt(i);
-      let j = orderCache.length - 1;
-      while (j >= 0) {
-        switch (close_bracket) {
-          case ")":
-            if (orderCache[j] == "(") {
-              orderCache.pop();
-            } else {
-              console.log("Incorrect order");
-              return false;
-            }
-            break;
-          case "}":
-            if (orderCache[j] == "{") {
-              orderCache.pop();
-            } else {
-              console.log("Incorrect order");
-              return false;
-            }
-            break;
-          case "]":
-            if (orderCache[j] == "[") {
-              orderCache.pop();
-            } else {
-              console.log("Incorrect order");
-              return false;
-            }
-            break;
-          default:
-            break;
-        }
-        j--;
-      }
+    let open_bracket = s.charAt(i);
+    switch (open_bracket) {
+      case "(":
+      case "{":
+      case "[":
+        isOpen = true;
+        orderCache.push(open_bracket);
     }
+    // else {
+    //   let close_bracket = s.charAt(i);
+    //   let j = orderCache.length - 1;
+    //   while (j >= 0) {
+    //     switch (close_bracket) {
+    //       case ")":
+    //         if (orderCache[j] == "(") {
+    //           orderCache.pop();
+    //         } else {
+    //           console.log("Incorrect order");
+    //           return false;
+    //         }
+    //         break;
+    //       case "}":
+    //         if (orderCache[j] == "{") {
+    //           orderCache.pop();
+    //         } else {
+    //           console.log("Incorrect order");
+    //           return false;
+    //         }
+    //         break;
+    //       case "]":
+    //         if (orderCache[j] == "[") {
+    //           orderCache.pop();
+    //         } else {
+    //           console.log("Incorrect order");
+    //           return false;
+    //         }
+    //         break;
+    //       default:
+    //         break;
+    //     }
+    //     j--;
+    //   }
+    // }
   }
+  return orderCache;
   return true;
 
   //   for (let i = s.length - 1; i >= 0; i--){
