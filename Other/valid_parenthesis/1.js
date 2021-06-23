3cb12c6ed469c276151e104c25aaa80091e95aa3
Abelard Bautista <abelardbautista@gmail.com>
2021-06-22 20:58:36 -0700

    Added logic.

--- Other/valid_parenthesis/1.js
+++ Other/valid_parenthesis/1.js
@@ -3,62 +3,43 @@
  * @return {boolean}
  */
 
-const s = "()";
+const s = "{[]}";
 var isValid = function (s) {
   let isOpen = false;
   let orderCache = [];
 
   for (let i = 0; i < s.length; i++) {
-    let open_bracket = s.charAt(i);
-    switch (open_bracket) {
+    let current_char = s.charAt(i);
+    switch (current_char) {
       case "(":
       case "{":
       case "[":
         isOpen = true;
-        orderCache.push(open_bracket);
+        orderCache.push(current_char);
+        break;
+      case ")":
+        if (orderCache[orderCache.length - 1] == "(") {
+          orderCache.pop();
+        } else {
+          return false;
+        }
+        break;
+      case "}":
+        if (orderCache[orderCache.length - 1] == "{") {
+          orderCache.pop();
+        } else {
+          return false;
+        }
+        break;
+      case "]":
+        if (orderCache[orderCache.length - 1] == "[") {
+          orderCache.pop();
+        } else {
+          return false;
+        }
+        break;
     }
-    // else {
-    //   let close_bracket = s.charAt(i);
-    //   let j = orderCache.length - 1;
-    //   while (j >= 0) {
-    //     switch (close_bracket) {
-    //       case ")":
-    //         if (orderCache[j] == "(") {
-    //           orderCache.pop();
-    //         } else {
-    //           console.log("Incorrect order");
-    //           return false;
-    //         }
-    //         break;
-    //       case "}":
-    //         if (orderCache[j] == "{") {
-    //           orderCache.pop();
-    //         } else {
-    //           console.log("Incorrect order");
-    //           return false;
-    //         }
-    //         break;
-    //       case "]":
-    //         if (orderCache[j] == "[") {
-    //           orderCache.pop();
-    //         } else {
-    //           console.log("Incorrect order");
-    //           return false;
-    //         }
-    //         break;
-    //       default:
-    //         break;
-    //     }
-    //     j--;
-    //   }
-    // }
   }
-  return orderCache;
   return true;
-
-  //   for (let i = s.length - 1; i >= 0; i--){
-  // if ()
-  //   }
-  //   return orderCache;
 };
 console.log(isValid(s));
