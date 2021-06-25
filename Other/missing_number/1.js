56f1bcf51fbdae031fded8d56e98ce735debfdea
Abelard Bautista <abelardbautista@gmail.com>
2021-06-24 18:52:06 -0700

    Solution for edge case when missing number == 0.

--- Other/missing_number/1.js
+++ Other/missing_number/1.js
@@ -2,11 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-const nums = [3, 0, 1];
+const nums = [1];
 var missingNumber = function (nums) {
   let n = nums.length;
-
   let sorted_nums = nums.sort();
+
+  if (sorted_nums[0] != 0) {
+    return 0;
+  }
+
   for (let i = 0; i <= n; i++) {
     if (sorted_nums[i + 1] - sorted_nums[i] != 1) {
       return sorted_nums[i] + 1;
