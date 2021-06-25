43847005757535e01b05480e8884a68cfb9cdcb7
Abelard Bautista <abelardbautista@gmail.com>
2021-06-24 18:45:47 -0700

    Sort then find missing value using difference between sequential values.

--- Other/missing_number/1.js
+++ Other/missing_number/1.js
@@ -2,4 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function (nums) {};
+const nums = [3, 0, 1];
+var missingNumber = function (nums) {
+  let n = nums.length;
+
+  let sorted_nums = nums.sort();
+  for (let i = 0; i <= n; i++) {
+    if (sorted_nums[i + 1] - sorted_nums[i] != 1) {
+      return sorted_nums[i] + 1;
+    }
+  }
+};
+console.log(missingNumber(nums));
