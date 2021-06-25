c3a1527b2c633ae30b2ab796bb6ea0b191c86189
Abelard Bautista <abelardbautista@gmail.com>
2021-06-24 19:00:05 -0700

    Debugged sorting issue.

--- Other/missing_number/1.js
+++ Other/missing_number/1.js
@@ -2,10 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-const nums = [1];
+const nums = [
+  45, 35, 38, 13, 12, 23, 48, 15, 44, 21, 43, 26, 6, 37, 1, 19, 22, 3, 11, 32,
+  4, 16, 28, 49, 29, 36, 33, 8, 9, 39, 46, 17, 41, 7, 2, 5, 27, 20, 40, 34, 30,
+  25, 47, 0, 31, 42, 24, 10, 14,
+];
 var missingNumber = function (nums) {
   let n = nums.length;
-  let sorted_nums = nums.sort();
+  let sorted_nums = nums.sort((a, b) => a - b);
 
   if (sorted_nums[0] != 0) {
     return 0;
