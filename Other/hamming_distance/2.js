6320ede78c75c72815e8be3578a39f2362759667
Abelard Bautista <abelardbautista@gmail.com>
2021-06-29 22:27:07 -0700

    Added logic.

--- Other/hamming_distance/2.js
+++ Other/hamming_distance/2.js
@@ -3,9 +3,23 @@
  * @param {number} y
  * @return {number}
  */
-const x = 4;
-const y = 14;
+const x = 3;
+const y = 1;
 
-var hammingDistance = function (x, y) {};
+var hammingDistance = function (x, y) {
+  let bit_x = x.toString(2);
+  let bit_y = y.toString(2);
+
+  let combined_int = parseInt(bit_x) + parseInt(bit_y);
+
+  let count = 0;
+  while (combined_int >= 1) {
+    if (combined_int % 10 == 1) {
+      count++;
+    }
+    combined_int = Math.floor(combined_int / 10);
+  }
+  return count;
+};
 
 console.log(hammingDistance(x, y));
