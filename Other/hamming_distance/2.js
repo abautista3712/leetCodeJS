0d3a9f1b42c18d609dba0874181aa1d1ddcaae72
Abelard Bautista <abelardbautista@gmail.com>
2021-06-29 21:43:17 -0700

    Started new approach.

---
+++ Other/hamming_distance/2.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+const x = 4;
+const y = 14;
+
+var hammingDistance = function (x, y) {};
+
+console.log(hammingDistance(x, y));
