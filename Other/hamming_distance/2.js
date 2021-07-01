16d71f78c89809bfc4abc98ddf9e63bd899a34bf
Abelard Bautista <abelardbautista@gmail.com>
2021-06-30 22:43:17 -0700

    Different approach to solve large int edge case.

--- Other/hamming_distance/2.js
+++ Other/hamming_distance/2.js
@@ -3,23 +3,49 @@
  * @param {number} y
  * @return {number}
  */
-const x = 3;
-const y = 1;
+const x = 1577962638;
+const y = 1727613287;
 
 var hammingDistance = function (x, y) {
   let bit_x = x.toString(2);
   let bit_y = y.toString(2);
 
-  let combined_int = parseInt(bit_x) + parseInt(bit_y);
+  let distance = 0;
 
-  let count = 0;
-  while (combined_int >= 1) {
-    if (combined_int % 10 == 1) {
-      count++;
-    }
-    combined_int = Math.floor(combined_int / 10);
+  if (bit_x.length != bit_y.length) {
+    distance = Math.abs(bit_x.length - bit_y.length);
   }
-  return count;
+
+  const adj = distance;
+
+  if (bit_x.length > bit_y.length) {
+    for (let i = 0; i < bit_y.length; i++) {
+      if (bit_x.charAt(i + adj) != bit_y.charAt(i)) {
+        distance++;
+      }
+    }
+  } else if (bit_y.length > bit_x.length) {
+    for (let i = 0; i < bit_x.length; i++) {
+      if (bit_y.charAt(i + adj) != bit_x.charAt(i)) {
+        distance++;
+      }
+    }
+  }
+
+  return distance;
+
+  //   let combined_int = parseInt(bit_x) + parseInt(bit_y);
+
+  //   console.log(combined_int.toString());
+
+  //   let count = 0;
+  //   while (combined_int >= 1) {
+  //     if (combined_int % 10 == 1) {
+  //       count++;
+  //     }
+  //     combined_int = Math.floor(combined_int / 10);
+  //   }
+  //   return count;
 };
 
 console.log(hammingDistance(x, y));
