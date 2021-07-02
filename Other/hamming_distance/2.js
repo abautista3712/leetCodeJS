5e4d3701c4e2b65bfb0caf41024bc994aa0c12f6
Abelard Bautista <abelardbautista@gmail.com>
2021-07-01 21:54:22 -0700

    Debugged and added notes.

--- Other/hamming_distance/2.js
+++ Other/hamming_distance/2.js
@@ -3,8 +3,8 @@
  * @param {number} y
  * @return {number}
  */
-const x = 1577962638;
-const y = 1727613287;
+const x = 1;
+const y = 4;
 
 var hammingDistance = function (x, y) {
   let bit_x = x.toString(2);
@@ -12,27 +12,41 @@ var hammingDistance = function (x, y) {
 
   let distance = 0;
 
+  //   Adjustment value: Length difference b/w bit_x and bit_y
   if (bit_x.length != bit_y.length) {
-    distance = Math.abs(bit_x.length - bit_y.length);
+    adj = Math.abs(bit_x.length - bit_y.length);
   }
 
-  const adj = distance;
-
+  //   When bit_x is LONGER / bit_y is SHORTER
   if (bit_x.length > bit_y.length) {
-    for (let i = 0; i < bit_y.length; i++) {
-      if (bit_x.charAt(i + adj) != bit_y.charAt(i)) {
+    for (let i = 0; i < adj; i++) {
+      if (bit_x.charAt(i) != 0) {
         distance++;
       }
     }
-  } else if (bit_y.length > bit_x.length) {
-    for (let i = 0; i < bit_x.length; i++) {
-      if (bit_y.charAt(i + adj) != bit_x.charAt(i)) {
+    for (let i = adj; i < bit_x.length; i++) {
+      if (bit_x.charAt(i) != bit_y.charAt(i)) {
         distance++;
       }
     }
-  } else {
+  }
+  //   When bit_y is LONGER / bit_x is SHORTER
+  else if (bit_y.length > bit_x.length) {
+    for (let i = 0; i < adj; i++) {
+      if (bit_y.charAt(i) != 0) {
+        distance++;
+      }
+    }
+    for (let i = adj; i < bit_y.length; i++) {
+      if (bit_y.charAt(i) != bit_x.charAt(i)) {
+        distance++;
+      }
+    }
+  }
+  //   When bit_x.length == bit_y.length
+  else {
     for (let i = 0; i < bit_x.length; i++) {
-      if (bit_y.charAt(i + adj) != bit_x.charAt(i)) {
+      if (bit_y.charAt(i) != bit_x.charAt(i)) {
         distance++;
       }
     }
